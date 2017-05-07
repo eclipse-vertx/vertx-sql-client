55613c1191c135759fcf3d6d23c75e40ca939ff1
Julien Viet <julien@julienviet.com>
2017-05-07 14:56:48 +0200

    Add reconnect tests

--- src/test/java/io/vertx/pgclient/PooledPgTest.java
+++ src/test/java/io/vertx/pgclient/PooledPgTest.java
@@ -4,6 +4,9 @@ import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import org.junit.Test;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
@@ -37,6 +40,56 @@ public class PooledPgTest extends PgTestBase {
         });
       }));
     }
+  }
+
+  @Test
+  public void testReconnect(TestContext ctx) {
+    Async async = ctx.async();
+    ProxyServer proxy = ProxyServer.create(vertx, options.getPort(), options.getHost());
+    AtomicReference<ProxyServer.Connection> proxyConn = new AtomicReference<>();
+    proxy.proxyHandler(conn -> {
+      proxyConn.set(conn);
+      conn.connect();
+    });
+    proxy.listen(8080, "localhost", ctx.asyncAssertSuccess(v1 -> {
+      PostgresClient client = PostgresClient.create(vertx, new PostgresClientOptions(options).setPort(8080).setHost("localhost"));
+      PostgresConnectionPool pool = client.createPool(1);
+      pool.getConnection(ctx.asyncAssertSuccess(conn1 -> {
+        proxyConn.get().close();
+        conn1.closeHandler(v2 -> {
+          conn1.execute("never-executer", ctx.asyncAssertFailure(err -> {
+            pool.getConnection(ctx.asyncAssertSuccess(conn2 -> {
+              conn2.execute("SELECT id, randomnumber from WORLD", ctx.asyncAssertSuccess(v3 -> {
+                async.complete();
+              }));
+            }));
+          }));
+        });
+      }));
+    }));
+  }
+
+  @Test
+  public void testReconnectQueued(TestContext ctx) {
+    Async async = ctx.async();
+    ProxyServer proxy = ProxyServer.create(vertx, options.getPort(), options.getHost());
+    AtomicReference<ProxyServer.Connection> proxyConn = new AtomicReference<>();
+    proxy.proxyHandler(conn -> {
+      proxyConn.set(conn);
+      conn.connect();
+    });
+    proxy.listen(8080, "localhost", ctx.asyncAssertSuccess(v1 -> {
+      PostgresClient client = PostgresClient.create(vertx, new PostgresClientOptions(options).setPort(8080).setHost("localhost"));
+      PostgresConnectionPool pool = client.createPool(1);
+      pool.getConnection(ctx.asyncAssertSuccess(conn -> {
+        proxyConn.get().close();
+      }));
+      pool.getConnection(ctx.asyncAssertSuccess(conn -> {
+        conn.execute("SELECT id, randomnumber from WORLD", ctx.asyncAssertSuccess(v2 -> {
+          async.complete();
+        }));
+      }));
+    }));
   }
 
   @Override
