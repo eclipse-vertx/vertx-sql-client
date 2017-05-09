eb7ad42e2f7e595d282e83bab26cb4db5b88d90f
Emad Alblueshi <emad.albloushi@gmail.com>
2017-05-09 16:05:16 +0300

    Add Transaction status for ReadyForQueryMessage

---
+++ src/main/java/io/vertx/pgclient/codec/decoder/TransactionStatus.java
@@ -0,0 +1,13 @@
+package io.vertx.pgclient.codec.decoder;
+
+/**
+ *
+ * @author <a href="mailto:emad.albloushi@gmail.com">Emad Alblueshi</a>
+ *
+ */
+
+public enum TransactionStatus {
+  NOT_BLOCK,
+  BLOCK,
+  FAILED
+}
