4a0a48f49758f82861b6c9af576753b02d5a2076
Emad Alblueshi <emad.albloushi@gmail.com>
2017-05-13 17:47:11 +0300

    simplify write

--- src/main/java/io/vertx/pgclient/codec/util/Util.java
+++ src/main/java/io/vertx/pgclient/codec/util/Util.java
@@ -7,14 +7,14 @@ import java.nio.charset.Charset;
 
 public class Util {
 
-  public static String readCString(ByteBuf buffer, Charset charset) {
-    byte[] bytes = new byte[buffer.bytesBefore((byte) 0) + 1];
-    buffer.readBytes(bytes);
+  public static String readCString(ByteBuf in, Charset charset) {
+    byte[] bytes = new byte[in.bytesBefore((byte) 0) + 1];
+    in.readBytes(bytes);
     return new String(bytes, 0, bytes.length - 1, charset);
   }
 
-  public static void writeCString(ByteBuf buffer, String input, Charset charset) {
-    buffer.writeBytes(input.getBytes(charset));
-    buffer.writeByte(0);
+  public static void writeCString(ByteBuf in, String input, Charset charset) {
+    in.writeCharSequence(input, charset);
+    in.writeByte(0);
   }
 }
