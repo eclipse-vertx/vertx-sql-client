56758940ebc578dcb10d3e04881a08e5525105a3
Emad Alblueshi <emad.albloushi@gmail.com>
2017-05-18 18:43:32 +0300

    Add extended Query method

--- src/main/java/io/vertx/pgclient/PostgresConnection.java
+++ src/main/java/io/vertx/pgclient/PostgresConnection.java
@@ -3,14 +3,19 @@ package io.vertx.pgclient;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 
+import java.util.List;
+
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
+
 public interface PostgresConnection {
 
   void exceptionHandler(Handler<Throwable> handler);
 
   void execute(String sql, Handler<AsyncResult<Result>> handler);
+
+  void execute(String sql, List<Object> params, Handler<AsyncResult<Result>> handler);
 
   void closeHandler(Handler<Void> handler);
 
