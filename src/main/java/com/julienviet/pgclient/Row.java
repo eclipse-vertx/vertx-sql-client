d49e2158f60ac0bf97504ae229bd7ce1b5096223
Emad Alblueshi <emad.albloushi@gmail.com>
2017-05-23 23:09:42 +0300

    Remove old Result and Row objects

--- src/main/java/com/julienviet/pgclient/Row.java
+++
@@ -1,9 +0,0 @@
-package com.julienviet.pgclient;
-
-import java.util.ArrayList;
-
-/**
- * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
- */
-public class Row extends ArrayList<Object> {
-}
