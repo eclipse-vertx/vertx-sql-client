6d1c4f052232f572112ab34c22b98d01ac6bde94
Emad Alblueshi <emad.albloushi@gmail.com>
2017-05-30 13:10:38 +0300

    Support tx management

--- src/main/java/com/julienviet/pgclient/impl/PostgresSQLConnection.java
+++ src/main/java/com/julienviet/pgclient/impl/PostgresSQLConnection.java
@@ -14,13 +14,18 @@ import io.vertx.ext.sql.UpdateResult;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ * @author <a href="mailto:emad.albloushi@gmail.com">Emad Alblueshi</a>
  */
 public class PostgresSQLConnection implements SQLConnection {
 
   private static final Logger log = LoggerFactory.getLogger(PostgresSQLConnection.class);
+
+  private AtomicBoolean inTransaction = new AtomicBoolean(false);
+  private AtomicBoolean inAutoCommit = new AtomicBoolean(true);
 
   private final DbConnection conn;
 
@@ -30,17 +35,49 @@ public class PostgresSQLConnection implements SQLConnection {
 
   @Override
   public SQLConnection setAutoCommit(boolean autoCommit, Handler<AsyncResult<Void>> handler) {
+    inAutoCommit.set(autoCommit);
+    if (inTransaction.compareAndSet(true, false) && autoCommit) {
+      conn.schedule(new QueryCommand("COMMIT", new ResultSetBuilder(re -> {
+        if(re.failed()) {
+          handler.handle(Future.failedFuture(re.cause()));
+        } else {
+          handler.handle(Future.succeededFuture());
+        }
+      })));
+    } else {
+      handler.handle(Future.succeededFuture());
+    }
+    return this;
+  }
+
+  @Override
+  public SQLConnection execute(String sql, Handler<AsyncResult<Void>> handler) {
     throw new UnsupportedOperationException();
   }
 
   @Override
-  public SQLConnection execute(String s, Handler<AsyncResult<Void>> handler) {
-    throw new UnsupportedOperationException();
+  public SQLConnection query(String sql, Handler<AsyncResult<ResultSet>> handler) {
+    beginTxIfNeeded(tx -> {
+      if(tx.failed()) {
+        handler.handle(Future.failedFuture(tx.cause()));
+      } else {
+        conn.schedule(new QueryCommand(sql, new ResultSetBuilder(handler)));
+      }
+    });
+    return this;
   }
 
   @Override
-  public SQLConnection query(String s, Handler<AsyncResult<ResultSet>> handler) {
-    conn.schedule(new QueryCommand(s, new ResultSetBuilder(handler)));
+  public SQLConnection queryWithParams(String sql, JsonArray jsonArray, Handler<AsyncResult<ResultSet>> handler) {
+    beginTxIfNeeded(tx -> {
+      if(tx.failed()) {
+        handler.handle(Future.failedFuture(tx.cause()));
+      } else {
+        conn.schedule(new PreparedQueryCommand(sql, jsonArray.getList(), new PreparedQueryResultHandler(ar -> {
+          handler.handle(ar.map(results -> results));
+        })));
+      }
+    });
     return this;
   }
 
@@ -50,61 +87,56 @@ public class PostgresSQLConnection implements SQLConnection {
   }
 
   @Override
-  public SQLConnection queryWithParams(String s, JsonArray jsonArray, Handler<AsyncResult<ResultSet>> handler) {
-    CommandBase cmd = new PreparedQueryCommand(s, jsonArray.getList(), new PreparedQueryResultHandler(ar -> {
-      handler.handle(ar.map(results -> results));
-    }));
-    conn.schedule(cmd);
-    return this;
-  }
-
-  @Override
-  public SQLConnection queryStreamWithParams(String s, JsonArray jsonArray, Handler<AsyncResult<SQLRowStream>> handler) {
+  public SQLConnection queryStreamWithParams(String sql, JsonArray jsonArray, Handler<AsyncResult<SQLRowStream>> handler) {
     throw new UnsupportedOperationException();
   }
 
   @Override
-  public SQLConnection update(String s, Handler<AsyncResult<UpdateResult>> handler) {
-    conn.schedule(new UpdateCommand(s, handler));
+  public SQLConnection update(String sql, Handler<AsyncResult<UpdateResult>> handler) {
+    beginTxIfNeeded(tx -> {
+      if(tx.failed()) {
+        handler.handle(Future.failedFuture(tx.cause()));
+      } else {
+        conn.schedule(new UpdateCommand(sql, handler));
+      }
+    });
     return this;
   }
 
   @Override
   public SQLConnection updateWithParams(String sql, JsonArray jsonArray, Handler<AsyncResult<UpdateResult>> handler) {
-    conn.schedule(new PreparedUpdateCommand(sql, Collections.singletonList(jsonArray.getList()), ar -> {
-      handler.handle(ar.map(results -> results.get(0)));
-    }));
+    beginTxIfNeeded(tx -> {
+      if(tx.failed()) {
+        handler.handle(Future.failedFuture(tx.cause()));
+      } else {
+        conn.schedule(new PreparedUpdateCommand(sql, Collections.singletonList(jsonArray.getList()), ar -> {
+          handler.handle(ar.map(results -> results.get(0)));
+        }));
+      }
+    });
     return this;
   }
 
   @Override
-  public SQLConnection call(String s, Handler<AsyncResult<ResultSet>> handler) {
+  public SQLConnection call(String sql, Handler<AsyncResult<ResultSet>> handler) {
     throw new UnsupportedOperationException();
   }
 
   @Override
-  public SQLConnection callWithParams(String s, JsonArray jsonArray, JsonArray out, Handler<AsyncResult<ResultSet>> handler) {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public void close(Handler<AsyncResult<Void>> handler) {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public void close() {
+  public SQLConnection callWithParams(String sql, JsonArray jsonArray, JsonArray out, Handler<AsyncResult<ResultSet>> handler) {
     throw new UnsupportedOperationException();
   }
 
   @Override
   public SQLConnection commit(Handler<AsyncResult<Void>> handler) {
-    throw new UnsupportedOperationException();
+    endAndBeginTx("COMMIT", handler);
+    return this;
   }
 
   @Override
   public SQLConnection rollback(Handler<AsyncResult<Void>> handler) {
-    throw new UnsupportedOperationException();
+    endAndBeginTx("ROLLBACK", handler);
+    return this;
   }
 
   @Override
@@ -141,5 +173,65 @@ public class PostgresSQLConnection implements SQLConnection {
   public SQLConnection getTransactionIsolation(Handler<AsyncResult<TransactionIsolation>> handler) {
     conn.schedule(new PreparedTxQueryCommand(handler));
     return this;
+  }
+
+  @Override
+  public void close(Handler<AsyncResult<Void>> handler) {
+    inAutoCommit.set(true);
+    if (inTransaction.compareAndSet(true, false)) {
+      conn.schedule(new QueryCommand("COMMIT", new ResultSetBuilder(re -> {
+        if(re.failed()) {
+          handler.handle(Future.failedFuture(re.cause()));
+        } else {
+          // should we use close command for open portal if any ?
+          conn.doClose();
+          handler.handle(Future.succeededFuture());
+        }
+      })));
+    } else {
+      conn.doClose();
+      handler.handle(Future.succeededFuture());
+    }
+  }
+
+  @Override
+  public void close() {
+    close(c -> {
+    });
+  }
+
+  private void beginTxIfNeeded(Handler<AsyncResult<Void>> handler) {
+    if (!inAutoCommit.get() && inTransaction.compareAndSet(false, true)) {
+      conn.schedule(new QueryCommand("BEGIN", new ResultSetBuilder(re -> {
+        if(re.failed()) {
+          handler.handle(Future.failedFuture(re.cause()));
+        } else {
+          handler.handle(Future.succeededFuture());
+        }
+      })));
+    } else {
+      handler.handle(Future.succeededFuture());
+    }
+  }
+
+  private void endAndBeginTx(String command, Handler<AsyncResult<Void>> handler) {
+    if (inTransaction.compareAndSet(true, false)) {
+      conn.schedule(new QueryCommand(command, new ResultSetBuilder(re -> {
+        if(re.failed()) {
+          handler.handle(Future.failedFuture(re.cause()));
+        } else {
+          conn.schedule(new QueryCommand("BEGIN", new ResultSetBuilder(rs -> {
+            if(rs.failed()) {
+              handler.handle(Future.failedFuture(rs.cause()));
+            } else {
+              inTransaction.compareAndSet(false, true);
+              handler.handle(Future.succeededFuture());
+            }
+          })));
+        }
+      })));
+    } else {
+      handler.handle(Future.failedFuture(new IllegalStateException("Not in transaction currently")));
+    }
   }
 }
