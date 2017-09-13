3a82eeb2bb158beb49639d9b5f2a55063d6359ca
Julien Viet <julien@julienviet.com>
2017-09-13 14:05:29 +0200

    Fix test that were missing the db config params

--- src/main/kotlin/com/julienviet/kotlin/pgclient/PgPoolOptions.kt
+++ src/main/kotlin/com/julienviet/kotlin/pgclient/PgPoolOptions.kt
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2017 Julien Viet
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.julienviet.kotlin.pgclient
 
 import com.julienviet.pgclient.PgPoolOptions
@@ -25,8 +8,8 @@ import com.julienviet.pgclient.PoolingMode
  *
  * The options for configuring a connection pool.
  *
- * @param maxSize
- * @param mode
+ * @param maxSize 
+ * @param mode 
  *
  * <p/>
  * NOTE: This function has been automatically generated from the [com.julienviet.pgclient.PgPoolOptions original] using Vert.x codegen.
