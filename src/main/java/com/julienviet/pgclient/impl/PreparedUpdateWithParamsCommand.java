679b75fa2f8d70ce3ccf17b336dc1abd41b7c73f
Emad Alblueshi <emad.albloushi@gmail.com>
2017-09-13 20:08:03 +0300

    Add Prepared update with params command

---
+++ src/main/java/com/julienviet/pgclient/impl/PreparedUpdateWithParamsCommand.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2017 Julien Viet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.julienviet.pgclient.impl;
+
+import com.julienviet.pgclient.codec.Message;
+import com.julienviet.pgclient.codec.decoder.message.BindComplete;
+import com.julienviet.pgclient.codec.decoder.message.NoData;
+import com.julienviet.pgclient.codec.decoder.message.ParameterDescription;
+import com.julienviet.pgclient.codec.decoder.message.ParseComplete;
+import com.julienviet.pgclient.codec.decoder.message.ReadyForQuery;
+import com.julienviet.pgclient.codec.encoder.message.Bind;
+import com.julienviet.pgclient.codec.encoder.message.Describe;
+import com.julienviet.pgclient.codec.encoder.message.Execute;
+import com.julienviet.pgclient.codec.encoder.message.Parse;
+import com.julienviet.pgclient.codec.encoder.message.Sync;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.ext.sql.UpdateResult;
+
+import java.util.List;
+
+import static com.julienviet.pgclient.codec.util.Util.*;
+
+/**
+ * @author <a href="mailto:emad.albloushi@gmail.com">Emad Alblueshi</a>
+ */
+
+class PreparedUpdateWithParamsCommand extends UpdateCommandBase {
+
+
+  final boolean parse;
+  final String sql;
+  final String stmt;
+  final List<Object> params;
+  final Handler<AsyncResult<UpdateResult>> handler;
+
+  PreparedUpdateWithParamsCommand(String sql, List<Object> params, Handler<AsyncResult<UpdateResult>> handler) {
+    this(true, sql, "", params, handler);
+  }
+
+  PreparedUpdateWithParamsCommand(boolean parse, String sql, String stmt, List<Object> params, Handler<AsyncResult<UpdateResult>> handler) {
+    this.parse = parse;
+    this.sql = sql;
+    this.stmt = stmt;
+    this.params = params;
+    this.handler = handler;
+  }
+
+  @Override
+  void exec(DbConnection conn, Handler<Void> handler) {
+    doneHandler = handler;
+    if (parse) {
+      conn.writeMessage(new Parse(sql).setStatement(stmt));
+    }
+    conn.writeMessage(new Bind().setParamValues(paramValues(params)).setStatement(stmt));
+    conn.writeMessage(new Describe().setStatement(stmt));
+    conn.writeMessage(new Execute().setRowCount(0));
+    conn.writeMessage(Sync.INSTANCE);
+  }
+
+  @Override
+  public void handleMessage(Message msg) {
+    if (msg.getClass() == ReadyForQuery.class) {
+      doneHandler.handle(null);
+    } else if (msg.getClass() == ParameterDescription.class) {
+    } else if (msg.getClass() == NoData.class) {
+    } else if (msg.getClass() == ParseComplete.class) {
+    } else if (msg.getClass() == BindComplete.class) {
+    } else {
+      super.handleMessage(msg);
+    }
+  }
+
+  @Override
+  void handleResult(UpdateResult result) {
+    handler.handle(Future.succeededFuture(result));
+  }
+
+  @Override
+  void fail(Throwable cause) {
+    handler.handle(Future.failedFuture(cause));
+  }
+}
