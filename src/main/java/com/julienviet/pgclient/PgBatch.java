6e2c6a94dbb5ca3ba62495542d272bf2e1016293
Julien Viet <julien@julienviet.com>
2017-09-13 10:59:29 +0200

    More javadoc

--- src/main/java/com/julienviet/pgclient/PgBatch.java
+++ src/main/java/com/julienviet/pgclient/PgBatch.java
@@ -11,44 +11,118 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * A batch execution.
+ *
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 @VertxGen
 public interface PgBatch {
 
+  /**
+   * Add a command with a no arguments to this batch
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  @Fluent
+  default PgBatch add() {
+    return add(Collections.emptyList());
+  }
+
+  /**
+   * Add a command with a single argument to this batch
+   *
+   * @param param1 the first argument of the command
+   * @return a reference to this, so the API can be used fluently
+   */
   @Fluent
   default PgBatch add(Object param1) {
     return add(Collections.singletonList(param1));
   }
 
+  /**
+   * Add a command with two arguments to this batch
+   *
+   * @param param1 the first argument of the command
+   * @param param2 the second argument of the command
+   * @return a reference to this, so the API can be used fluently
+   */
   @Fluent
   default PgBatch add(Object param1, Object param2) {
     return add(Arrays.asList(param1, param2));
   }
 
+  /**
+   * Add a command with three arguments to this batch
+   *
+   * @param param1 the first argument of the command
+   * @param param2 the second argument of the command
+   * @param param3 the third argument of the command
+   * @return a reference to this, so the API can be used fluently
+   */
   @Fluent
   default PgBatch add(Object param1, Object param2, Object param3) {
     return add(Arrays.asList(param1, param2, param3));
   }
 
+  /**
+   * Add a command with four arguments to this batch
+   *
+   * @param param1 the first argument of the command
+   * @param param2 the second argument of the command
+   * @param param3 the third argument of the command
+   * @param param4 the fourth argument of the command
+   * @return a reference to this, so the API can be used fluently
+   */
   @Fluent
   default PgBatch add(Object param1, Object param2, Object param3, Object param4) {
     return add(Arrays.asList(param1, param2, param3, param4));
   }
 
+  /**
+   * Add a command with five arguments to this batch
+   *
+   * @param param1 the first argument of the command
+   * @param param2 the second argument of the command
+   * @param param3 the third argument of the command
+   * @param param4 the fourth argument of the command
+   * @param param5 the fifth argument of the command
+   * @return a reference to this, so the API can be used fluently
+   */
   @Fluent
   default PgBatch add(Object param1, Object param2, Object param3, Object param4, Object param5) {
     return add(Arrays.asList(param1, param2, param3, param4, param5));
   }
 
+  /**
+   * Add a command with six arguments to this batch
+   *
+   * @param param1 the first argument of the command
+   * @param param2 the second argument of the command
+   * @param param3 the third argument of the command
+   * @param param4 the fourth argument of the command
+   * @param param5 the fifth argument of the command
+   * @param param6 the sixth argument of the command
+   * @return a reference to this, so the API can be used fluently
+   */
   @Fluent
   default PgBatch add(Object param1, Object param2, Object param3, Object param4, Object param5, Object param6) {
     return add(Arrays.asList(param1, param2, param3, param4, param5, param6));
   }
 
+  /**
+   * Add a command with a variable number of arguments to this batch
+   *
+   * @param params the arguments of the command
+   * @return a reference to this, so the API can be used fluently
+   */
   @Fluent
   PgBatch add(List<Object> params);
 
+  /**
+   * Execute the batch and notifies {@code resultHandler} of the result.
+   *
+   * @param resultHandler the handler of the result
+   */
   void execute(Handler<AsyncResult<List<UpdateResult>>> resultHandler);
 
 }
--- src/main/java/com/julienviet/pgclient/PgPreparedStatement.java
+++ src/main/java/com/julienviet/pgclient/PgPreparedStatement.java
@@ -8,76 +8,103 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * A postgres prepared statement.
+ *
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 public interface PgPreparedStatement {
 
+  /**
+   * @return create a query from this statement with no arguments
+   */
   default PgQuery query() {
     return query(Collections.emptyList());
   }
 
+  /**
+   * @param param1 the first argument of the query
+   * @return create a query from this statement with one argument
+   */
   default PgQuery query(Object param1) {
     return query(Collections.singletonList(param1));
   }
 
+  /**
+   * @param param1 the first argument of the query
+   * @param param2 the second argument of the query
+   * @return create a query from this statement with two arguments
+   */
   default PgQuery query(Object param1, Object param2) {
     return query(Arrays.asList(param1, param2));
   }
 
+  /**
+   * @param param1 the first argument of the query
+   * @param param2 the second argument of the query
+   * @param param3 the third argument of the query
+   * @return create a query from this statement with three arguments
+   */
   default PgQuery query(Object param1, Object param2, Object param3) {
     return query(Arrays.asList(param1, param2, param3));
   }
 
+  /**
+   * @param param1 the first argument of the query
+   * @param param2 the second argument of the query
+   * @param param3 the third argument of the query
+   * @param param4 the fourth argument of the query
+   * @return create a query from this statement with four arguments
+   */
   default PgQuery query(Object param1, Object param2, Object param3, Object param4) {
     return query(Arrays.asList(param1, param2, param3, param4));
   }
 
+  /**
+   * @param param1 the first argument of the query
+   * @param param2 the second argument of the query
+   * @param param3 the third argument of the query
+   * @param param4 the fourth argument of the query
+   * @param param5 the fifth argument of the query
+   * @return create a query from this statement with five arguments
+   */
   default PgQuery query(Object param1, Object param2, Object param3, Object param4, Object param5) {
     return query(Arrays.asList(param1, param2, param3, param4, param5));
   }
 
+  /**
+   * @param param1 the first argument of the query
+   * @param param2 the second argument of the query
+   * @param param3 the third argument of the query
+   * @param param4 the fourth argument of the query
+   * @param param5 the fifth argument of the query
+   * @param param6 the sixth argument of the query
+   * @return create a query from this statement with six arguments
+   */
   default PgQuery query(Object param1, Object param2, Object param3, Object param4, Object param5, Object param6) {
     return query(Arrays.asList(param1, param2, param3, param4, param5, param6));
   }
 
+  /**
+   * @param params the list of arguments
+   * @return create a query from this statement with a variable list of arguments
+   */
   PgQuery query(List<Object> params);
 
-/*
-  default PgRowStream queryStream() {
-    return queryStream(Collections.emptyList());
-  }
-
-  default PgRowStream queryStream(Object param1) {
-    return queryStream(Collections.singletonList(param1));
-  }
-
-  default PgRowStream queryStream(Object param1, Object param2) {
-    return queryStream(Arrays.asList(param1, param2));
-  }
-
-  default PgRowStream queryStream(Object param1, Object param2, Object param3) {
-    return queryStream(Arrays.asList(param1, param2, param3));
-  }
-
-  default PgRowStream queryStream(Object param1, Object param2, Object param3, Object param4) {
-    return queryStream(Arrays.asList(param1, param2, param3, param4));
-  }
-
-  default PgRowStream queryStream(Object param1, Object param2, Object param3, Object param4, Object param5) {
-    return queryStream(Arrays.asList(param1, param2, param3, param4, param5));
-  }
-
-  default PgRowStream queryStream(Object param1, Object param2, Object param3, Object param4, Object param5, Object param6) {
-    return queryStream(Arrays.asList(param1, param2, param3, param4, param5, param6));
-  }
-
-  PgRowStream queryStream(List<Object> params);
-*/
-
+  /**
+   * Create a new batch.
+   *
+   * @return the batch
+   */
   PgBatch batch();
 
+  /**
+   * Close the prepared statement and release its resources.
+   */
   void close();
 
+  /**
+   * Like {@link #close()} but notifies the {@code completionHandler} when it's closed.
+   */
   void close(Handler<AsyncResult<Void>> completionHandler);
 
 }
