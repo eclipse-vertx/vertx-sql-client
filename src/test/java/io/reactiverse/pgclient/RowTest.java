7e41e1fa763645609c2f33aef6e5ec41d8f047a0
Emad Alblueshi <emad.albloushi@gmail.com>
2018-07-20 17:45:57 +0300

    Adds user exception tests for pooled and non pooled connection

--- src/test/java/io/reactiverse/pgclient/RowTest.java
+++ src/test/java/io/reactiverse/pgclient/RowTest.java
@@ -45,7 +45,7 @@ public class RowTest extends PgTestBase {
   }
 
   @Test
-  public void testGetNonExistingRows(TestContext ctx) {
+  public void testGetNonExistingRowsNonPooled(TestContext ctx) {
     Async async = ctx.async();
     PgClient.connect(vertx, options, ctx.asyncAssertSuccess(conn -> {
       conn.query("SELECT 1 \"foo\"",
@@ -97,6 +97,160 @@ public class RowTest extends PgTestBase {
     }));
   }
 
+  @Test
+  public void testOutOfRangePosNonPooled(TestContext ctx) {
+    Async async = ctx.async();
+    PgClient.connect(vertx, options, ctx.asyncAssertSuccess(conn -> {
+      conn.query("SELECT 1",
+        ctx.asyncAssertSuccess(result -> {
+          Row row = result.iterator().next();
+          List<Function<Integer, ?>> functions = Arrays.asList(
+            row::getValue,
+            row::getString,
+            row::getBuffer,
+            row::getDouble,
+            row::getShort,
+            row::getInteger,
+            row::getLong,
+            row::getBigDecimal,
+            row::getNumeric,
+            row::getFloat,
+            row::getLocalDate,
+            row::getLocalTime,
+            row::getOffsetDateTime,
+            row::getLocalDateTime,
+            row::getOffsetTime,
+            row::getTemporal,
+            row::getUUID,
+            row::getBooleanArray,
+            row::getShortArray,
+            row::getIntegerArray,
+            row::getLongArray,
+            row::getFloatArray,
+            row::getDoubleArray,
+            row::getStringArray,
+            row::getLocalDateArray,
+            row::getLocalTimeArray,
+            row::getOffsetTimeArray,
+            row::getLocalDateTimeArray,
+            row::getOffsetDateTimeArray,
+            row::getBufferArray,
+            row::getUUIDArray
+          );
+          functions.forEach(f -> {
+            try {
+              f.apply(Integer.MAX_VALUE);
+              ctx.fail("Index out of bounds exception");
+            } catch (IndexOutOfBoundsException ignore) {}
+          });
+          async.complete();
+        }));
+    }));
+  }
 
+  @Test
+  public void testGetNonExistingRowsPooled(TestContext ctx) {
+    Async async = ctx.async();
+    PgPool pgPool = PgClient.pool(vertx, new PgPoolOptions(options).setMaxSize(1));
+    pgPool.getConnection(ctx.asyncAssertSuccess(conn -> {
+      conn.query("SELECT 1 \"foo\"",
+        ctx.asyncAssertSuccess(result -> {
+          Row row = result.iterator().next();
+          List<Function<String, ?>> functions = Arrays.asList(
+            row::getValue,
+            row::getString,
+            row::getBuffer,
+            row::getDouble,
+            row::getShort,
+            row::getInteger,
+            row::getLong,
+            row::getBigDecimal,
+            row::getNumeric,
+            row::getFloat,
+            row::getLocalDate,
+            row::getLocalTime,
+            row::getOffsetDateTime,
+            row::getLocalDateTime,
+            row::getOffsetTime,
+            row::getTemporal,
+            row::getUUID,
+            row::getBooleanArray,
+            row::getShortArray,
+            row::getIntegerArray,
+            row::getLongArray,
+            row::getFloatArray,
+            row::getDoubleArray,
+            row::getStringArray,
+            row::getLocalDateArray,
+            row::getLocalTimeArray,
+            row::getOffsetTimeArray,
+            row::getLocalDateTimeArray,
+            row::getOffsetDateTimeArray,
+            row::getBufferArray,
+            row::getUUIDArray
+          );
+          functions.forEach(f -> {
+            ctx.assertEquals(null, f.apply("bar"));
+            try {
+              f.apply(null);
+              ctx.fail("Was expecting an NPE");
+            } catch (NullPointerException ignore) {
+            }
+          });
+          async.complete();
+        }));
+    }));
+  }
 
+  @Test
+  public void testOutOfRangePooled(TestContext ctx) {
+    Async async = ctx.async();
+    PgPool pgPool = PgClient.pool(vertx, new PgPoolOptions(options).setMaxSize(1));
+    pgPool.getConnection(ctx.asyncAssertSuccess(conn -> {
+      conn.query("SELECT 1",
+        ctx.asyncAssertSuccess(result -> {
+          Row row = result.iterator().next();
+          List<Function<Integer, ?>> functions = Arrays.asList(
+            row::getValue,
+            row::getString,
+            row::getBuffer,
+            row::getDouble,
+            row::getShort,
+            row::getInteger,
+            row::getLong,
+            row::getBigDecimal,
+            row::getNumeric,
+            row::getFloat,
+            row::getLocalDate,
+            row::getLocalTime,
+            row::getOffsetDateTime,
+            row::getLocalDateTime,
+            row::getOffsetTime,
+            row::getTemporal,
+            row::getUUID,
+            row::getBooleanArray,
+            row::getShortArray,
+            row::getIntegerArray,
+            row::getLongArray,
+            row::getFloatArray,
+            row::getDoubleArray,
+            row::getStringArray,
+            row::getLocalDateArray,
+            row::getLocalTimeArray,
+            row::getOffsetTimeArray,
+            row::getLocalDateTimeArray,
+            row::getOffsetDateTimeArray,
+            row::getBufferArray,
+            row::getUUIDArray
+          );
+          functions.forEach(f -> {
+            try {
+              f.apply(Integer.MAX_VALUE);
+              ctx.fail("Index out of bounds exception");
+            } catch (IndexOutOfBoundsException ignore) {}
+          });
+          async.complete();
+        }));
+    }));
+  }
 }
