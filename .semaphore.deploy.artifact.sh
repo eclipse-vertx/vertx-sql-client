6794afad41b44f1a8a2c4b9902fcf479ff8b6711
Julien Viet <julien@julienviet.com>
2018-09-09 17:31:21 +0200

    Fix semaphore deploy script

--- .semaphore.deploy.artifact.sh
+++ .semaphore.deploy.artifact.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep -v '\[')
 if [[ "$PROJECT_VERSION" =~ .*SNAPSHOT ]] && [[ "${BRANCH_NAME}" = "master" ]] && [[ "${TRAVIS_PULL_REQUEST}" = "" ]];
 then
-   echo "SHOULD DEPLOY SNAPSHOT"
+   echo "SHOULD DEPLOY SNAPSHOT";
 #  mvn deploy -DskipTests -B;
 fi
