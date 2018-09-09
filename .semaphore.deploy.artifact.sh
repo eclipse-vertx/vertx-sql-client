38c20bb7725b2edf7b47cc42f5cb8fd1f836f07e
Julien Viet <julien@julienviet.com>
2018-09-09 17:20:54 +0200

    Fix semaphore deploy script

--- .semaphore.deploy.artifact.sh
+++ .semaphore.deploy.artifact.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep -v '\[')
 if [[ "$PROJECT_VERSION" =~ .*SNAPSHOT ]] && [[ "${BRANCH_NAME}" = "master" ]] && [[ "${TRAVIS_PULL_REQUEST}" = "" ]];
 then
