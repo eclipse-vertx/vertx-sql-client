1038f585259ea18b9a4ced5c11817890c6ad8447
Julien Viet <julien@julienviet.com>
2018-09-09 18:11:21 +0200

    Tweak semaphore deploy

--- .semaphore.deploy.artifact.sh
+++ .semaphore.deploy.artifact.sh
@@ -1,6 +1,5 @@
 PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep '^[^\[].*-SNAPSHOT')
 if [ -n "${PROJECT_VERSION}" ] && [ "${BRANCH_NAME}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "" ];
 then
-   echo "SHOULD DEPLOY SNAPSHOT";
-#  mvn deploy -DskipTests -B;
+  mvn deploy -DskipTests -B;
 fi
