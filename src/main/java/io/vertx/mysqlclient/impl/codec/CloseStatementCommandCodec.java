d047c05d080ffc1fad86aab8f97de33e8a632b7d
Billy Yuan <billy112487983@gmail.com>
2019-04-06 14:51:37 +0800

    add support for MySQL prepared-statement close & reset message codecs
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>
    
    Signed-off-by: Jeoffrey Lim <jeoffreyl@gmail.com>

---
+++ src/main/java/io/vertx/mysqlclient/impl/codec/CloseStatementCommandCodec.java
@@ -0,0 +1,33 @@
+package io.vertx.mysqlclient.impl.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.vertx.mysqlclient.impl.protocol.CommandType;
+import io.vertx.sqlclient.impl.command.CloseStatementCommand;
+import io.vertx.sqlclient.impl.command.CommandResponse;
+
+public class CloseStatementCommandCodec extends CommandCodec<Void, CloseStatementCommand> {
+
+  public CloseStatementCommandCodec(CloseStatementCommand cmd) {
+    super(cmd);
+  }
+
+  @Override
+  void encodePayload(MySQLEncoder encoder) {
+    super.encodePayload(encoder);
+    MySQLPreparedStatement ps = (MySQLPreparedStatement) cmd.ps();
+
+    ByteBuf payload = encoder.chctx.alloc().ioBuffer();
+
+    payload.writeByte(CommandType.COM_STMT_CLOSE);
+    payload.writeIntLE((int) ps.statementId);
+
+    encoder.writePacketAndFlush(sequenceId++, payload);
+
+    completionHandler.handle(CommandResponse.success(null));
+  }
+
+  @Override
+  void decodePayload(ByteBuf payload, MySQLEncoder encoder, int payloadLength, int sequenceId) {
+    // no statement response
+  }
+}
