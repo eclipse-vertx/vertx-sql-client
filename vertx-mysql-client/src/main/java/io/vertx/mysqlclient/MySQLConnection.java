616b7aa311a5c5244d149fad2c579fd81ab6b6a6
Billy Yuan <billy112487983@gmail.com>
2019-06-02 22:06:42 +0800

    Project clean up (#309)
    * minor
    
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>
    
    * add MySQL interfaces javadoc
    
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>
    
    * add codegen annotations for MySQL interfaces
    
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>
    
    * add README for MySQL
    
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>
    
    * typos
    
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>
    
    * fix testing for travis
    
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>
    
    Signed-off-by: Jeoffrey Lim <jeoffreyl@gmail.com>

--- vertx-mysql-client/src/main/java/io/vertx/mysqlclient/MySQLConnection.java
+++ vertx-mysql-client/src/main/java/io/vertx/mysqlclient/MySQLConnection.java
@@ -1,5 +1,7 @@
 package io.vertx.mysqlclient;
 
+import io.vertx.codegen.annotations.GenIgnore;
+import io.vertx.codegen.annotations.VertxGen;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
@@ -13,7 +15,18 @@ import io.vertx.sqlclient.Tuple;
 
 import java.util.stream.Collector;
 
+/**
+ * A connection to MySQL server.
+ */
+@VertxGen
 public interface MySQLConnection extends SqlConnection {
+  /**
+   * Create a connection to MySQL server with the given {@code connectOptions}.
+   *
+   * @param vertx the vertx instance
+   * @param options the options for the connection
+   * @param handler the handler called with the connection or the failure
+   */
   static void connect(Vertx vertx, MySQLConnectOptions options, Handler<AsyncResult<MySQLConnection>> handler) {
     MySQLConnectionImpl.connect(vertx, options, handler);
   }
@@ -30,18 +43,21 @@ public interface MySQLConnection extends SqlConnection {
   @Override
   MySQLConnection preparedQuery(String sql, Handler<AsyncResult<RowSet>> handler);
 
+  @GenIgnore
   @Override
   <R> MySQLConnection preparedQuery(String sql, Collector<Row, ?, R> collector, Handler<AsyncResult<SqlResult<R>>> handler);
 
   @Override
   MySQLConnection query(String sql, Handler<AsyncResult<RowSet>> handler);
 
+  @GenIgnore
   @Override
   <R> MySQLConnection query(String sql, Collector<Row, ?, R> collector, Handler<AsyncResult<SqlResult<R>>> handler);
 
   @Override
   MySQLConnection preparedQuery(String sql, Tuple arguments, Handler<AsyncResult<RowSet>> handler);
 
+  @GenIgnore
   @Override
   <R> MySQLConnection preparedQuery(String sql, Tuple arguments, Collector<Row, ?, R> collector, Handler<AsyncResult<SqlResult<R>>> handler);
 
