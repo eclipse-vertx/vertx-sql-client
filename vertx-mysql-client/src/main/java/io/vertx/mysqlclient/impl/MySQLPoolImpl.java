8ca1caefe772251adb2c44e3514b1d19c3766616
Billy Yuan <billy112487983@gmail.com>
2019-06-16 12:59:05 +0800

    improve unsupported operation failure handling for MySQL
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>

--- vertx-mysql-client/src/main/java/io/vertx/mysqlclient/impl/MySQLPoolImpl.java
+++ vertx-mysql-client/src/main/java/io/vertx/mysqlclient/impl/MySQLPoolImpl.java
@@ -6,9 +6,17 @@ import io.vertx.core.Handler;
 import io.vertx.mysqlclient.MySQLConnectOptions;
 import io.vertx.mysqlclient.MySQLPool;
 import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.SqlResult;
+import io.vertx.sqlclient.Transaction;
+import io.vertx.sqlclient.Tuple;
 import io.vertx.sqlclient.impl.Connection;
 import io.vertx.sqlclient.impl.PoolBase;
 import io.vertx.sqlclient.impl.SqlConnectionImpl;
+
+import java.util.List;
+import java.util.stream.Collector;
 
 public class MySQLPoolImpl extends PoolBase<MySQLPoolImpl> implements MySQLPool {
   private final MySQLConnectionFactory factory;
@@ -26,5 +34,20 @@ public class MySQLPoolImpl extends PoolBase<MySQLPoolImpl> implements MySQLPool 
   @Override
   protected SqlConnectionImpl wrap(Context context, Connection conn) {
     return new MySQLConnectionImpl(factory, context, conn);
+  }
+
+  @Override
+  public void begin(Handler<AsyncResult<Transaction>> handler) {
+    throw new UnsupportedOperationException("Transaction is not supported for now");
+  }
+
+  @Override
+  public MySQLPoolImpl preparedBatch(String sql, List<Tuple> batch, Handler<AsyncResult<RowSet>> handler) {
+    throw new UnsupportedOperationException("PreparedBatch is not supported for now");
+  }
+
+  @Override
+  public <R> MySQLPoolImpl preparedBatch(String sql, List<Tuple> batch, Collector<Row, ?, R> collector, Handler<AsyncResult<SqlResult<R>>> handler) {
+    throw new UnsupportedOperationException("PreparedBatch is not supported for now");
   }
 }
