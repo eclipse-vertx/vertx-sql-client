6e8e593287e9f0ce56c0bbaf5f895b7f0dbd40aa
Billy Yuan <billy112487983@gmail.com>
2019-06-23 01:30:59 +0800

    add support for connection uri in MySQL
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>

---
+++ vertx-mysql-client/src/test/java/io/vertx/mysqlclient/MySQLConnectOptionsProviderTest.java
@@ -0,0 +1,120 @@
+package io.vertx.mysqlclient;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MySQLConnectOptionsProviderTest {
+  private String connectionUri;
+  private MySQLConnectOptions expectedConfiguration;
+  private MySQLConnectOptions actualConfiguration;
+
+  @Test
+  public void testValidUri1() {
+    connectionUri = "mysql://localhost";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+
+    expectedConfiguration = new MySQLConnectOptions();
+
+    assertEquals(expectedConfiguration, actualConfiguration);
+  }
+
+  @Test
+  public void testValidUri2() {
+    connectionUri = "mysql://myhost";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+
+    expectedConfiguration = new MySQLConnectOptions()
+      .setHost("myhost");
+
+    assertEquals(expectedConfiguration, actualConfiguration);
+  }
+
+  @Test
+  public void testValidUri3() {
+    connectionUri = "mysql://myhost:5433";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+
+    expectedConfiguration = new MySQLConnectOptions()
+      .setHost("myhost")
+      .setPort(3306);
+
+    assertEquals(expectedConfiguration, actualConfiguration);
+  }
+
+  @Test
+  public void testValidUri4() {
+    connectionUri = "mysql://myhost/mydb";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+
+    expectedConfiguration = new MySQLConnectOptions()
+      .setHost("myhost")
+      .setDatabase("mydb");
+
+    assertEquals(expectedConfiguration, actualConfiguration);
+  }
+
+  @Test
+  public void testValidUri5() {
+    connectionUri = "mysql://user@myhost";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+
+    expectedConfiguration = new MySQLConnectOptions()
+      .setUser("user")
+      .setHost("myhost");
+
+    assertEquals(expectedConfiguration, actualConfiguration);
+  }
+
+  @Test
+  public void testValidUri6() {
+    connectionUri = "mysql://user:secret@myhost";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+
+    expectedConfiguration = new MySQLConnectOptions()
+      .setUser("user")
+      .setPassword("secret")
+      .setHost("myhost");
+
+    assertEquals(expectedConfiguration, actualConfiguration);
+  }
+
+  @Test
+  public void testValidUri7() {
+    connectionUri = "mysql://other@localhost/otherdb?port=3306&password=secret";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+
+    expectedConfiguration = new MySQLConnectOptions()
+      .setUser("other")
+      .setPassword("secret")
+      .setHost("localhost")
+      .setPort(3306)
+      .setDatabase("otherdb");
+
+    assertEquals(expectedConfiguration, actualConfiguration);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidUri1() {
+    connectionUri = "mysql://username:password@loc//dbname";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidUri2() {
+    connectionUri = "mysql://user@:passowrd@localhost/dbname/qwer";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidUri3() {
+    connectionUri = "mysql://user:password@localhost:655355/dbname";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidUri4() {
+    connectionUri = "mysql://user@localhost?port=1234&port";
+    actualConfiguration = MySQLConnectOptions.fromUri(connectionUri);
+  }
+}
