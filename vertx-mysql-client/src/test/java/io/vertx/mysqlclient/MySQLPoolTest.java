c2523abb8c03d304219d1e9ee1680a08ef5629b0
Billy Yuan <billy112487983@gmail.com>
2019-07-27 19:23:26 +0800

    MySQL pool connection factory properties should not be mutated when creating new connections - fixes #365
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>

---
+++ vertx-mysql-client/src/test/java/io/vertx/mysqlclient/MySQLPoolTest.java
@@ -0,0 +1,58 @@
+package io.vertx.mysqlclient;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.sqlclient.PoolOptions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class MySQLPoolTest extends MySQLTestBase {
+
+  Vertx vertx;
+  MySQLConnectOptions options;
+  MySQLPool pool;
+
+  @Before
+  public void setup() {
+    vertx = Vertx.vertx();
+    options = new MySQLConnectOptions(MySQLTestBase.options);
+    pool = MySQLPool.pool(vertx, options, new PoolOptions());
+  }
+
+  @After
+  public void tearDown(TestContext ctx) {
+    vertx.close(ctx.asyncAssertSuccess());
+  }
+
+  @Test
+  public void testContinuouslyConnecting(TestContext ctx) {
+    Async async = ctx.async(3);
+    pool.getConnection(ctx.asyncAssertSuccess(conn1 -> async.countDown()));
+    pool.getConnection(ctx.asyncAssertSuccess(conn2 -> async.countDown()));
+    pool.getConnection(ctx.asyncAssertSuccess(conn3 -> async.countDown()));
+    async.await();
+  }
+
+  @Test
+  public void testContinuouslyQuery(TestContext ctx) {
+    Async async = ctx.async(3);
+    pool.preparedQuery("SELECT 1", ctx.asyncAssertSuccess(res1 -> {
+      ctx.assertEquals(1, res1.size());
+      async.countDown();
+    }));
+    pool.preparedQuery("SELECT 2", ctx.asyncAssertSuccess(res1 -> {
+      ctx.assertEquals(1, res1.size());
+      async.countDown();
+    }));
+    pool.preparedQuery("SELECT 3", ctx.asyncAssertSuccess(res1 -> {
+      ctx.assertEquals(1, res1.size());
+      async.countDown();
+    }));
+    async.await();
+  }
+}
