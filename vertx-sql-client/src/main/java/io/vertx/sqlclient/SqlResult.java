3ba7628e94292eecbf57f94a0179a6af15204215
Billy Yuan <billy112487983@gmail.com>
2019-08-13 21:47:17 +0800

    add javadoc for the SqlResult property
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>

--- vertx-sql-client/src/main/java/io/vertx/sqlclient/SqlResult.java
+++ vertx-sql-client/src/main/java/io/vertx/sqlclient/SqlResult.java
@@ -57,7 +57,14 @@ public interface SqlResult<T> {
    */
   int size();
 
-  <V> V property(PropertyKind<V> property);
+  /**
+   * Get the property with the specified {@link PropertyKind}.
+   *
+   * @param propertyKind the kind of the property
+   * @param <V> the type of the property value
+   * @return the value of the property
+   */
+  <V> V property(PropertyKind<V> propertyKind);
 
   /**
    * Get the result value.
