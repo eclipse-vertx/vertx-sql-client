706ede9f051a0bc300f292ae6e3bffeed579025b
Billy Yuan <billy112487983@gmail.com>
2019-09-03 20:14:54 +0800

    add support for TLS in MySQL
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>

--- vertx-mysql-client/src/main/generated/io/vertx/mysqlclient/MySQLConnectOptionsConverter.java
+++ vertx-mysql-client/src/main/generated/io/vertx/mysqlclient/MySQLConnectOptionsConverter.java
@@ -24,6 +24,11 @@ public class MySQLConnectOptionsConverter {
             obj.setCollation((String)member.getValue());
           }
           break;
+        case "sslMode":
+          if (member.getValue() instanceof String) {
+            obj.setSslMode(io.vertx.mysqlclient.SslMode.valueOf((String)member.getValue()));
+          }
+          break;
       }
     }
   }
@@ -38,6 +43,9 @@ public class MySQLConnectOptionsConverter {
     }
     if (obj.getCollation() != null) {
       json.put("collation", obj.getCollation());
+    }
+    if (obj.getSslMode() != null) {
+      json.put("sslMode", obj.getSslMode().name());
     }
   }
 }
