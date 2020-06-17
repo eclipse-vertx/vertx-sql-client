46c50bf1033ef7d98c7fbebd7cf381c506d258eb
Julien Viet <julien@julienviet.com>
2020-06-17 11:21:09 +0200

    ArrayTuple resizing multiply the capacity by 4 instead of doubling and adding 1 - fixes #680

--- vertx-sql-client/src/main/java/io/vertx/sqlclient/impl/ArrayTuple.java
+++ vertx-sql-client/src/main/java/io/vertx/sqlclient/impl/ArrayTuple.java
@@ -19,6 +19,7 @@ package io.vertx.sqlclient.impl;
 
 import io.vertx.sqlclient.Tuple;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class ArrayTuple implements TupleInternal {
@@ -57,7 +58,7 @@ public class ArrayTuple implements TupleInternal {
   @Override
   public Tuple addValue(Object value) {
     if (size >= values.length) {
-      Object[] copy = new Object[values.length << 1 + 1];
+      Object[] copy = new Object[(values.length << 1) + 1];
       System.arraycopy(values, 0, copy, 0, values.length);
       values = copy;
     }
@@ -72,19 +73,17 @@ public class ArrayTuple implements TupleInternal {
 
   @Override
   public void clear() {
-    for (int i = 0;i < values.length;i++) {
-      values[i] = null;
-    }
+    Arrays.fill(values, null);
     size = 0;
   }
 
   @Override
   public void setValue(int pos, Object value) {
     if (pos < 0) {
-      throw new IndexOutOfBoundsException("Invali position " + pos + ": must be > 0");
+      throw new IndexOutOfBoundsException("Invalid position " + pos + ": must be > 0");
     }
     if (pos > size) {
-      throw new IndexOutOfBoundsException("Invali position " + pos + ": must be < " + size);
+      throw new IndexOutOfBoundsException("Invalid position " + pos + ": must be < " + size);
     }
     values[pos] = value;
   }
