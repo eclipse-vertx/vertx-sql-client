524a2319a5c4d5ea6cdaec03d9c6462136e6fe06
Julien Viet <julien@julienviet.com>
2020-07-01 16:36:56 +0200

    Improve PostgreSQL version parsing - fixes #705 - fixes #697

--- vertx-pg-client/src/main/java/io/vertx/pgclient/impl/PgDatabaseMetadata.java
+++ vertx-pg-client/src/main/java/io/vertx/pgclient/impl/PgDatabaseMetadata.java
@@ -1,27 +1,36 @@
 package io.vertx.pgclient.impl;
 
-import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.vertx.sqlclient.spi.DatabaseMetadata;
 
 public class PgDatabaseMetadata implements DatabaseMetadata {
-  
+
+  private static final Pattern VERSION_PATTERN = Pattern.compile(
+    "^(\\d+)(?:\\.(\\d+))?");
+
   private final String fullVersion;
-  private final int majorVersion;
-  private final int minorVersion;
-  
+  private int majorVersion;
+  private int minorVersion;
+
   public PgDatabaseMetadata(String serverVersion) {
     fullVersion = serverVersion;
-    StringTokenizer versionTokens = new StringTokenizer(serverVersion, "."); // aaXbb.ccYdd
-    majorVersion = integerPart(versionTokens.nextToken()); // aaXbb
-    minorVersion = integerPart(versionTokens.nextToken()); // ccYdd
+    Matcher matcher = VERSION_PATTERN.matcher(serverVersion);
+    if (matcher.find()) {
+      majorVersion = Integer.parseInt(matcher.group(1));
+      String minorPart = matcher.group(2);
+      if (minorPart != null) {
+        minorVersion = Integer.parseInt(minorPart);
+      }
+    }
   }
-  
+
   @Override
   public String productName() {
     return "PostgreSQL";
   }
-  
+
   @Override
   public String fullVersion() {
     return fullVersion;
@@ -36,25 +45,4 @@ public class PgDatabaseMetadata implements DatabaseMetadata {
   public int minorVersion() {
     return minorVersion;
   }
-  
-  // Parse a "dirty" integer surrounded by non-numeric characters
-  private static int integerPart(String dirtyString) {
-    int start = 0;
-
-    while (start < dirtyString.length() && !Character.isDigit(dirtyString.charAt(start))) {
-      ++start;
-    }
-
-    int end = start;
-    while (end < dirtyString.length() && Character.isDigit(dirtyString.charAt(end))) {
-      ++end;
-    }
-
-    if (start == end) {
-      return 0;
-    }
-
-    return Integer.parseInt(dirtyString.substring(start, end));
-  }
-
 }
---
+++ vertx-pg-client/src/test/java/io/vertx/pgclient/PgDatabaseMetadataTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.pgclient;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.pgclient.junit.ContainerPgRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(VertxUnitRunner.class)
+public class PgDatabaseMetadataTest {
+
+  private Vertx vertx;
+
+  @Before
+  public void setUp() {
+    vertx = Vertx.vertx();
+  }
+
+  @After
+  public void tearDown(TestContext ctx) {
+    vertx.close(ctx.asyncAssertSuccess());
+  }
+
+  @Test
+  public void test1(TestContext ctx) throws Exception {
+    test(ctx, "9.6.18", "9.6.18", 9, 6);
+  }
+
+  @Test
+  public void test2(TestContext ctx) throws Exception {
+    test(ctx, "12.3", "12.3 (Debian 12.3-1.pgdg100+1)", 12, 3);
+  }
+
+  @Test
+  public void test3(TestContext ctx) throws Exception {
+    test(ctx, "13-beta2", "13beta2 (Debian 13~beta2-1.pgdg100+1)", 13, 0);
+  }
+
+  private void test(TestContext ctx,
+                    String containerVersion,
+                    String expectedFull, int expectedMajor,
+                    int expectedMinor) throws Exception {
+    ContainerPgRule rule = new ContainerPgRule();
+    Async async = ctx.async();
+    try {
+      PgConnectOptions options = rule.startServer(containerVersion);
+      PgConnection.connect(vertx, options, ctx.asyncAssertSuccess(conn -> {
+        ctx.assertEquals(expectedFull, conn.databaseMetadata().fullVersion());
+        ctx.assertEquals(expectedMajor, conn.databaseMetadata().majorVersion());
+        ctx.assertEquals(expectedMinor, conn.databaseMetadata().minorVersion());
+        async.complete();
+      }));
+      async.await(20_000);
+    } finally {
+      rule.stopServer();
+    }
+  }
+}
