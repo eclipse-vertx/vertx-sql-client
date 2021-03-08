8841c2477deb7ad9d21ae11e83fcd43b3f6d5dde
vladimir <smagellan@gmail.com>
2021-03-08 14:26:11 +0300

    added Decimal column support
    Signed-off-by: vladimir <smagellan@gmail.com>

--- vertx-clickhouse-native-client/src/main/java/io/vertx/clickhouse/clickhousenative/impl/codec/columns/ClickhouseColumns.java
+++ vertx-clickhouse-native-client/src/main/java/io/vertx/clickhouse/clickhousenative/impl/codec/columns/ClickhouseColumns.java
@@ -19,8 +19,8 @@ public class ClickhouseColumns {
   public static final String FIXED_STRING_PREFIX = "FixedString(";
   public static final int FIXED_STRING_PREFIX_LENGTH = FIXED_STRING_PREFIX.length();
 
-  private static final int DATETIME_COLUMN_WIDTH = DateTimeColumn.ELEMENT_SIZE;
-  private static final int DATETIME64_COLUMN_WIDTH = DateTime64Column.ELEMENT_SIZE;
+  public static final String DECIMAL_PREFIX = "Decimal(";
+  public static final int DECIMAL_PREFIX_LENGTH = DECIMAL_PREFIX.length();
 
   public static ClickhouseNativeColumnDescriptor columnDescriptorForSpec(String unparsedSpec, String name) {
     String spec = unparsedSpec;
@@ -49,20 +49,20 @@ public class ClickhouseColumns {
                                                                          boolean isLowCardinality) {
     boolean unsigned = spec.startsWith("U");
     if (spec.equals("UInt8") || spec.equals("Int8")) {
-      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, 1, JDBCType.TINYINT, nullable, unsigned, isLowCardinality,
+      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, UInt8Column.ELEMENT_SIZE, JDBCType.TINYINT, nullable, unsigned, isLowCardinality,
         unsigned ? 0 : -128, unsigned ? 255 : 127);
     } else if (spec.equals("UInt16") || spec.equals("Int16")) {
-      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, 2, JDBCType.SMALLINT, nullable, unsigned, isLowCardinality,
+      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, UInt16Column.ELEMENT_SIZE, JDBCType.SMALLINT, nullable, unsigned, isLowCardinality,
         unsigned ? 0 : -32768, unsigned ? 65535 : 32767);
     } if (spec.equals("UInt32") || spec.equals("Int32")) {
-      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec,  isArray, 4, JDBCType.INTEGER, nullable, unsigned, isLowCardinality,
+      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec,  isArray, UInt32Column.ELEMENT_SIZE, JDBCType.INTEGER, nullable, unsigned, isLowCardinality,
         unsigned ? 0 : -2147483648L, unsigned ? 4294967295L : 2147483647L);
     } if (spec.equals("UInt64") || spec.equals("Int64")) {
-      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec,  isArray, 8, JDBCType.BIGINT, nullable, unsigned, isLowCardinality,
+      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec,  isArray, UInt64Column.ELEMENT_SIZE, JDBCType.BIGINT, nullable, unsigned, isLowCardinality,
         unsigned ? BigInteger.ZERO : new BigInteger("-9223372036854775808"),
         unsigned ? new BigInteger("18446744073709551615") : new BigInteger("9223372036854775807"));
     } if (spec.equals("Int128")) {
-      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec,  isArray, 16, JDBCType.BIGINT, nullable, false, isLowCardinality,
+      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec,  isArray, UInt128Column.ELEMENT_SIZE, JDBCType.BIGINT, nullable, false, isLowCardinality,
         new BigInteger("-170141183460469231731687303715884105728"), new BigInteger( "170141183460469231731687303715884105727"));
     } else if (spec.equals("String")) {
       return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, ClickhouseNativeColumnDescriptor.NOSIZE, JDBCType.VARCHAR,
@@ -73,16 +73,35 @@ public class ClickhouseColumns {
       return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, bytesLength, JDBCType.VARCHAR,
         nullable, false, isLowCardinality, null, null);
     } else if (spec.equals("DateTime") || spec.startsWith("DateTime(")) {
-      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, DATETIME_COLUMN_WIDTH,
+      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, DateTimeColumn.ELEMENT_SIZE,
         spec.endsWith(")") ? JDBCType.TIMESTAMP_WITH_TIMEZONE : JDBCType.TIMESTAMP, nullable, false, isLowCardinality, null, null);
     } else if (spec.equals("DateTime64") || spec.startsWith("DateTime64(")) {
-      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, DATETIME64_COLUMN_WIDTH,
+      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, DateTime64Column.ELEMENT_SIZE,
         spec.endsWith(")") ? JDBCType.TIMESTAMP_WITH_TIMEZONE : JDBCType.TIMESTAMP, nullable, false, isLowCardinality, null, null);
     } else if (spec.equals("UUID")) {
-      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, 16,
+      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, UUIDColumn.ELEMENT_SIZE,
         JDBCType.OTHER, nullable, false, isLowCardinality, null, null);
+    } else if (spec.startsWith(DECIMAL_PREFIX)) {
+      String decimalModifiers = spec.substring(DECIMAL_PREFIX_LENGTH, spec.length() - 1);
+      String[] modifiersTokens = decimalModifiers.split(",");
+      int precision = Integer.parseInt(modifiersTokens[0].trim());
+      int scale = Integer.parseInt(modifiersTokens[1].trim());
+      return new ClickhouseNativeColumnDescriptor(name, unparsedSpec, spec, isArray, decimalSize(precision),
+        JDBCType.DECIMAL, nullable, false, isLowCardinality, null, null, precision, scale);
     }
     throw new IllegalArgumentException("unknown spec: '" + spec + "'");
+  }
+
+  private static int decimalSize(int precision) {
+    if (precision <= Decimal32Column.MAX_PRECISION) {
+      return Decimal32Column.ELEMENT_SIZE;
+    } else if (precision <= Decimal64Column.MAX_PRECISION) {
+      return Decimal64Column.ELEMENT_SIZE;
+    } else if (precision <= Decimal128Column.MAX_PRECISION) {
+      return Decimal128Column.ELEMENT_SIZE;
+    } else {
+      return Decimal256Column.ELEMENT_SIZE;
+    }
   }
 
   public static ClickhouseColumn columnForSpec(String name, ClickhouseNativeColumnDescriptor descr, int nRows) {
@@ -100,9 +119,9 @@ public class ClickhouseColumns {
       } else if (jdbcType == JDBCType.INTEGER) {
         return new UInt32Column(nRows, descr);
       } else if (jdbcType == JDBCType.BIGINT) {
-        if (descr.getElementSize() == 8) {
+        if (descr.getElementSize() == UInt64Column.ELEMENT_SIZE) {
           return new UInt64Column(nRows, descr);
-        } else if (descr.getElementSize() == 16) {
+        } else if (descr.getElementSize() == UInt128Column.ELEMENT_SIZE) {
           return new UInt128Column(nRows, descr);
         }
       } else if (jdbcType == JDBCType.VARCHAR) {
@@ -118,11 +137,11 @@ public class ClickhouseColumns {
         if (nativeType.endsWith(")")) {
           int openBracePos = nativeType.indexOf("(");
           String dateModifiers = nativeType.substring(openBracePos + 1, nativeType.length() - 1);
-          if (descr.getElementSize() == DATETIME64_COLUMN_WIDTH) {
+          if (descr.getElementSize() == DateTime64Column.ELEMENT_SIZE) {
             String[] modifiers = dateModifiers.split(",");
-            precision = Integer.parseInt(modifiers[0]);
+            precision = Integer.parseInt(modifiers[0].trim());
             zoneId = modifiers.length == 2
-              ? ZoneId.of(modifiers[1])
+              ? ZoneId.of(modifiers[1].trim())
               : ZoneId.systemDefault();
           } else {
             zoneId = ZoneId.of(dateModifiers);
@@ -131,6 +150,17 @@ public class ClickhouseColumns {
           zoneId = ZoneId.systemDefault();
         }
         return precision == null ? new DateTimeColumn(nRows, descr, zoneId) : new DateTime64Column(nRows, descr, precision, zoneId);
+      } else if (jdbcType == JDBCType.DECIMAL) {
+        //TODO smagellan: merge into one statement after introducing column readers
+        if (descr.getElementSize() == Decimal32Column.ELEMENT_SIZE) {
+          return new Decimal32Column(nRows, descr);
+        } else if (descr.getElementSize() == Decimal64Column.ELEMENT_SIZE) {
+          return new Decimal64Column(nRows, descr);
+        } else if (descr.getElementSize() == Decimal128Column.ELEMENT_SIZE) {
+          return new Decimal128Column(nRows, descr);
+        } else if (descr.getElementSize() == Decimal256Column.ELEMENT_SIZE) {
+          return new Decimal256Column(nRows, descr);
+        }
       } else if (jdbcType == JDBCType.OTHER && descr.getNativeType().equals("UUID")) {
         return new UUIDColumn(nRows, descr);
       }
---
+++ vertx-clickhouse-native-client/src/main/java/io/vertx/clickhouse/clickhousenative/impl/codec/columns/Decimal32Column.java
@@ -0,0 +1,34 @@
+package io.vertx.clickhouse.clickhousenative.impl.codec.columns;
+
+import io.vertx.clickhouse.clickhousenative.impl.codec.ClickhouseNativeColumnDescriptor;
+import io.vertx.clickhouse.clickhousenative.impl.codec.ClickhouseStreamDataSource;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+//TODO smagellan: maybe introduce separate universal column reader/column
+public class Decimal32Column extends ClickhouseColumn {
+  public static final int ELEMENT_SIZE = 4;
+  public static final int MAX_PRECISION = 9;
+  public static final MathContext MATH_CONTEXT = new MathContext(MAX_PRECISION, RoundingMode.HALF_EVEN);
+
+  protected Decimal32Column(int nRows, ClickhouseNativeColumnDescriptor columnDescriptor) {
+    super(nRows, columnDescriptor);
+  }
+
+  @Override
+  protected Object readItems(ClickhouseStreamDataSource in) {
+    if (in.readableBytes() >= ELEMENT_SIZE * nRows) {
+      BigDecimal[] data = new BigDecimal[nRows];
+      int scale = columnDescriptor.getScale();
+      for (int i = 0; i < nRows; ++i) {
+        int item = in.readIntLE();
+        data[i] = new BigDecimal(BigInteger.valueOf(item), scale, MATH_CONTEXT);
+      }
+      return data;
+    }
+    return null;
+  }
+}
