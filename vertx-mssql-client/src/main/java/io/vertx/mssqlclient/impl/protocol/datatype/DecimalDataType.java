4f7ce54949839c472f4939e51df19f2d5ae6b8e1
Thomas Segismont <tsegismont@gmail.com>
2021-05-07 14:49:28 +0200

    MS SQL numeric/decimal column support with BigDecimal
    The support for these types was originally implemented using the SQL Client Numeric Java type.
    
    The Numeric Java type has been created because Postgres numeric/decimal columns can have the NaN value.
    
    MS SQL doesn't have it so we can simplify things and work with BigDecimal.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-mssql-client/src/main/java/io/vertx/mssqlclient/impl/protocol/datatype/DecimalDataType.java
+++ vertx-mssql-client/src/main/java/io/vertx/mssqlclient/impl/protocol/datatype/DecimalDataType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ * Copyright (c) 2011-2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -14,12 +14,12 @@ package io.vertx.mssqlclient.impl.protocol.datatype;
 import java.sql.JDBCType;
 
 // NUMERIC, NUMERICN, DECIMAL, or DECIMALN.
-public class NumericDataType extends MSSQLDataType {
+public class DecimalDataType extends MSSQLDataType {
   private final int precision;
   private final int scale;
 
-  public NumericDataType(int id, Class<?> mappedJavaType, int precision, int scale) {
-    super(id, mappedJavaType, JDBCType.NUMERIC);
+  public DecimalDataType(int id, Class<?> mappedJavaType, int precision, int scale) {
+    super(id, mappedJavaType, JDBCType.DECIMAL);
     this.precision = precision;
     this.scale = scale;
   }
