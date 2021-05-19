1bb8ec9f0041d850f93972cf499dacb7431b2fe8
Julien Viet <julien@julienviet.com>
2021-05-19 10:03:52 +0200

    The text array element end detector fails to find the end the second element {foo,bar\}, the parsed string by the data type codec is {foo,"bar\\"}. When the current detector handles a backslash char, it does not check if it was already escaped and thus the escaped backslash does escapes the ending quotes.
    When the end detector handles a backslash it should switch to escaped flag only if it is was not previously escaped.
    
    fixes #958
    fixes #957

--- vertx-pg-client/src/test/java/io/vertx/pgclient/data/CharacterTypesSimpleCodecTest.java
+++ vertx-pg-client/src/test/java/io/vertx/pgclient/data/CharacterTypesSimpleCodecTest.java
@@ -1,9 +1,14 @@
 package io.vertx.pgclient.data;
 
+import io.vertx.ext.unit.Async;
+import io.vertx.pgclient.PgConnection;
+import io.vertx.sqlclient.ColumnChecker;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.Tuple;
 import io.vertx.ext.unit.TestContext;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class CharacterTypesSimpleCodecTest extends SimpleQueryDataTypeCodecTestBase {
   @Test
@@ -48,6 +53,14 @@ public class CharacterTypesSimpleCodecTest extends SimpleQueryDataTypeCodecTestB
   }
 
   @Test
+  public void testDecodeTEXTArray2(TestContext ctx) {
+    // \ is escaped when reading the text array and we get {foo,"bar\\"}
+    // as it is the last char of the second element we need to ensure
+    // that the self escape (\\) does not escape the ending "
+    testDecodeGenericArray(ctx, "ARRAY ['foo' :: TEXT, 'bar\\' :: TEXT]", "TextArray", Tuple::getArrayOfStrings, Row::getArrayOfStrings, "foo", "bar\\");
+  }
+
+  @Test
   public void testDecodeVARCHARArray(TestContext ctx) {
     testDecodeGenericArray(ctx, "ARRAY ['Knock, knock.Who’s there?very long pause….Java.' :: VARCHAR]", "VarcharArray", Tuple::getArrayOfStrings, Row::getArrayOfStrings, "Knock, knock.Who’s there?very long pause….Java.");
   }
@@ -55,5 +68,33 @@ public class CharacterTypesSimpleCodecTest extends SimpleQueryDataTypeCodecTestB
   @Test
   public void testDecodeNAMEArray(TestContext ctx) {
     testDecodeGenericArray(ctx, "ARRAY ['Knock, knock.Who’s there?very long pause….Java.' :: NAME]", "NameArray", Tuple::getArrayOfStrings, Row::getArrayOfStrings, "Knock, knock.Who’s there?very long pause….Java.");
+  }
+
+  @Test
+  public void testFoo(TestContext ctx) {
+    PgConnection.connect(vertx, options, ctx.asyncAssertSuccess(conn -> {
+      conn.query("CREATE TABLE test2 (col text[])")
+        .execute()
+        .flatMap(res ->
+        conn.query("INSERT INTO test2 VALUES (ARRAY['foo','b\\ar'])").execute()
+      ).flatMap(v ->
+        conn.query("SELECT * FROM test2").execute()
+      ).onComplete(ctx.asyncAssertSuccess(res -> {
+        Row next = res.iterator().next();
+        System.out.println(Arrays.asList(next.getArrayOfStrings("col")));
+      }));
+      /*
+      conn.query("SET TIME ZONE 'UTC'").execute(
+        ctx.asyncAssertSuccess(res -> {
+          conn.query("SELECT " + arrayData + " \"" + columnName + "\"").execute(ctx.asyncAssertSuccess(result -> {
+            ctx.assertEquals(1, result.size());
+            Row row = result.iterator().next();
+            checker.forRow(row);
+            async.complete();
+          }));
+        }));
+
+       */
+    }));
   }
 }
