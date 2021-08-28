b79fbe728be932158978b3449e9dbe99d00b00ae
Mark Swatosh <mark.swatosh@gmail.com>
2021-08-28 11:51:57 -0500

    Ignore comments in Queries

--- vertx-db2-client/src/test/java/io/vertx/db2client/QueryVariationsTest.java
+++ vertx-db2-client/src/test/java/io/vertx/db2client/QueryVariationsTest.java
@@ -221,6 +221,33 @@ public class QueryVariationsTest extends DB2TestBase {
       }));
     }));
   }
+  
+  /**
+   * Test comment in query
+   */
+  @Test
+  public void testComment(TestContext ctx) {
+    connect(ctx.asyncAssertSuccess(con -> {
+      con.query("SELECT id,message FROM immutable " +
+          "WHERE message IN " +
+          "(SELECT message FROM immutable WHERE id = '4' OR id = '7') -- comment").execute(
+        ctx.asyncAssertSuccess(rowSet -> {
+        ctx.assertEquals(2, rowSet.size());
+        con.query("SELECT id,message FROM immutable " +
+            "WHERE message IN " +
+            "(SELECT message FROM immutable WHERE id = '4' OR id = '7') /* test comment */").execute(
+          ctx.asyncAssertSuccess(rowSet2 -> {
+          ctx.assertEquals(2, rowSet.size());
+          con.query("/* /* test comment */*/ /* /* Overly */ /*Complicated*/ /* Comment  */*/SELECT id,message FROM immutable " +
+              "WHERE message IN " +
+              "(SELECT message FROM immutable WHERE id = '4' OR id = '7')").execute(
+            ctx.asyncAssertSuccess(rowSet3 -> {
+            ctx.assertEquals(2, rowSet.size());
+          }));
+        }));
+      }));
+    }));
+  }
 
   private int assertSequenceResult(TestContext ctx, RowSet<Row> rowSet, Consumer<Integer> validation) {
     ctx.assertEquals(1, rowSet.size());
