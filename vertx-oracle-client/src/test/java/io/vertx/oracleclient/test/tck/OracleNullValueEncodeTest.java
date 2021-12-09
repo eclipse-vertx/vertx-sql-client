a14559406e3894d18aa796a17a0d56cc69e11e02
Thomas Segismont <tsegismont@gmail.com>
2021-12-09 22:39:20 +0100

    Implement OracleNullValueEncodeTest
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

---
+++ vertx-oracle-client/src/test/java/io/vertx/oracleclient/test/tck/OracleNullValueEncodeTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2011-2021 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.oracleclient.test.tck;
+
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.oracleclient.test.junit.OracleRule;
+import io.vertx.sqlclient.tck.NullValueEncodeTestBase;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class OracleNullValueEncodeTest extends NullValueEncodeTestBase {
+
+  @ClassRule
+  public static OracleRule rule = OracleRule.SHARED_INSTANCE;
+
+  @Override
+  protected void initConnector() {
+    connector = ClientConfig.POOLED.connect(vertx, rule.options());
+  }
+
+  @Override
+  protected String statement(String... parts) {
+    return "SELECT ? FROM dual";
+  }
+}
