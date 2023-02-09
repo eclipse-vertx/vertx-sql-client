f197b0867c6ca73d222c87700bd263729f9ad149
Pull Vert <5107035+pull-vert@users.noreply.github.com>
2023-02-09 23:00:46 +0100

    Oracle : Fix java.time.* generated values fix #1276

---
+++ vertx-oracle-client/src/test/java/io/vertx/oracleclient/test/OracleGeneratedDefaultValuesPooledTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2011-2023 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+
+package io.vertx.oracleclient.test;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.sqlclient.SqlClient;
+
+import java.util.function.Function;
+
+public class OracleGeneratedDefaultValuesPooledTest extends OracleGeneratedDefaultValuesTestBase {
+
+  @Override
+  protected <T> void withSqlClient(Function<SqlClient, Future<T>> function, Handler<AsyncResult<T>> handler) {
+    function.apply(pool).onComplete(handler);
+  }
+}
