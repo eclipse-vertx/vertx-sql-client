9849887679348ad7faab047d9b0b9fa23a76059e
BillyYccc <billy112487983@gmail.com>
2023-07-22 15:41:46 +0800

    MySQL client data type mapping rework - fixes #1239

--- vertx-mysql-client/src/main/java/io/vertx/mysqlclient/impl/protocol/ColumnDefinition.java
+++ vertx-mysql-client/src/main/java/io/vertx/mysqlclient/impl/protocol/ColumnDefinition.java
@@ -63,7 +63,7 @@ public final class ColumnDefinition implements ColumnDescriptor {
 
   @Override
   public JDBCType jdbcType() {
-    return type.jdbcType;
+    return type.getJdbcType();
   }
 
   @Override
@@ -82,33 +82,33 @@ public final class ColumnDefinition implements ColumnDescriptor {
    */
   @SuppressWarnings("unused")
   public static final class ColumnType {
-    public static final int MYSQL_TYPE_DECIMAL = 0x00;
-    public static final int MYSQL_TYPE_TINY = 0x01;
-    public static final int MYSQL_TYPE_SHORT = 0x02;
-    public static final int MYSQL_TYPE_LONG = 0x03;
-    public static final int MYSQL_TYPE_FLOAT = 0x04;
-    public static final int MYSQL_TYPE_DOUBLE = 0x05;
-    public static final int MYSQL_TYPE_NULL = 0x06;
-    public static final int MYSQL_TYPE_TIMESTAMP = 0x07;
-    public static final int MYSQL_TYPE_LONGLONG = 0x08;
-    public static final int MYSQL_TYPE_INT24 = 0x09;
-    public static final int MYSQL_TYPE_DATE = 0x0A;
-    public static final int MYSQL_TYPE_TIME = 0x0B;
-    public static final int MYSQL_TYPE_DATETIME = 0x0C;
-    public static final int MYSQL_TYPE_YEAR = 0x0D;
-    public static final int MYSQL_TYPE_VARCHAR = 0x0F;
-    public static final int MYSQL_TYPE_BIT = 0x10;
-    public static final int MYSQL_TYPE_JSON = 0xF5;
-    public static final int MYSQL_TYPE_NEWDECIMAL = 0xF6;
-    public static final int MYSQL_TYPE_ENUM = 0xF7;
-    public static final int MYSQL_TYPE_SET = 0xF8;
-    public static final int MYSQL_TYPE_TINY_BLOB = 0xF9;
-    public static final int MYSQL_TYPE_MEDIUM_BLOB = 0xFA;
-    public static final int MYSQL_TYPE_LONG_BLOB = 0xFB;
-    public static final int MYSQL_TYPE_BLOB = 0xFC;
-    public static final int MYSQL_TYPE_VAR_STRING = 0xFD;
-    public static final int MYSQL_TYPE_STRING = 0xFE;
-    public static final int MYSQL_TYPE_GEOMETRY = 0xFF;
+    public static final short MYSQL_TYPE_DECIMAL = 0x00;
+    public static final short MYSQL_TYPE_TINY = 0x01;
+    public static final short MYSQL_TYPE_SHORT = 0x02;
+    public static final short MYSQL_TYPE_LONG = 0x03;
+    public static final short MYSQL_TYPE_FLOAT = 0x04;
+    public static final short MYSQL_TYPE_DOUBLE = 0x05;
+    public static final short MYSQL_TYPE_NULL = 0x06;
+    public static final short MYSQL_TYPE_TIMESTAMP = 0x07;
+    public static final short MYSQL_TYPE_LONGLONG = 0x08;
+    public static final short MYSQL_TYPE_INT24 = 0x09;
+    public static final short MYSQL_TYPE_DATE = 0x0A;
+    public static final short MYSQL_TYPE_TIME = 0x0B;
+    public static final short MYSQL_TYPE_DATETIME = 0x0C;
+    public static final short MYSQL_TYPE_YEAR = 0x0D;
+    public static final short MYSQL_TYPE_VARCHAR = 0x0F;
+    public static final short MYSQL_TYPE_BIT = 0x10;
+    public static final short MYSQL_TYPE_JSON = 0xF5;
+    public static final short MYSQL_TYPE_NEWDECIMAL = 0xF6;
+    public static final short MYSQL_TYPE_ENUM = 0xF7;
+    public static final short MYSQL_TYPE_SET = 0xF8;
+    public static final short MYSQL_TYPE_TINY_BLOB = 0xF9;
+    public static final short MYSQL_TYPE_MEDIUM_BLOB = 0xFA;
+    public static final short MYSQL_TYPE_LONG_BLOB = 0xFB;
+    public static final short MYSQL_TYPE_BLOB = 0xFC;
+    public static final short MYSQL_TYPE_VAR_STRING = 0xFD;
+    public static final short MYSQL_TYPE_STRING = 0xFE;
+    public static final short MYSQL_TYPE_GEOMETRY = 0xFF;
 
     /*
       Internal to MySQL Server
--- vertx-mysql-client/src/test/java/io/vertx/mysqlclient/tck/MySQLBinaryDataTypeDecodeTest.java
+++ vertx-mysql-client/src/test/java/io/vertx/mysqlclient/tck/MySQLBinaryDataTypeDecodeTest.java
@@ -125,6 +125,81 @@ public class MySQLBinaryDataTypeDecodeTest extends BinaryDataTypeDecodeTestBase 
           ctx.assertEquals(Duration.ofHours(18).plusMinutes(45).plusSeconds(2), row.getValue("test_time"));
           conn.close();
         }));
-      }));
-    }
+    }));
+  }
+
+  @Test
+  @Override
+  public void testFloat4(TestContext ctx) {
+    testDecodeGeneric(ctx, "test_float_4", Float.class, JDBCType.FLOAT, 3.40282e38F);
+  }
+
+  @Test
+  @Override
+  public void testChar(TestContext ctx) {
+    testDecodeGeneric(ctx, "test_char", String.class, JDBCType.CHAR, "testchar");
+  }
+
+  @Test
+  public void test_jdbc_type(TestContext ctx) {
+    connector.connect(ctx.asyncAssertSuccess(conn -> {
+      conn
+        .query("SELECT * from datatype where id = 1")
+        .execute()
+        .onComplete(ctx.asyncAssertSuccess(result -> {
+          ctx.assertEquals(1, result.size());
+
+          ctx.assertEquals(result.columnDescriptors().get(0).name(), "id");
+          ctx.assertEquals(result.columnDescriptors().get(0).jdbcType(), JDBCType.INTEGER);
+          ctx.assertEquals(result.columnDescriptors().get(1).name(), "Binary");
+          ctx.assertEquals(result.columnDescriptors().get(1).jdbcType(), JDBCType.BINARY);
+          ctx.assertEquals(result.columnDescriptors().get(2).name(), "VarBinary");
+          ctx.assertEquals(result.columnDescriptors().get(2).jdbcType(), JDBCType.VARBINARY);
+          ctx.assertEquals(result.columnDescriptors().get(3).name(), "TinyBlob");
+          ctx.assertEquals(result.columnDescriptors().get(3).jdbcType(), JDBCType.BLOB);
+          ctx.assertEquals(result.columnDescriptors().get(4).name(), "Blob");
+          ctx.assertEquals(result.columnDescriptors().get(4).jdbcType(), JDBCType.BLOB);
+          ctx.assertEquals(result.columnDescriptors().get(5).name(), "MediumBlob");
+          ctx.assertEquals(result.columnDescriptors().get(5).jdbcType(), JDBCType.BLOB);
+          ctx.assertEquals(result.columnDescriptors().get(6).name(), "LongBlob");
+          ctx.assertEquals(result.columnDescriptors().get(6).jdbcType(), JDBCType.BLOB);
+          ctx.assertEquals(result.columnDescriptors().get(7).name(), "TinyText");
+          ctx.assertEquals(result.columnDescriptors().get(7).jdbcType(), JDBCType.CLOB);
+          ctx.assertEquals(result.columnDescriptors().get(8).name(), "Text");
+          ctx.assertEquals(result.columnDescriptors().get(8).jdbcType(), JDBCType.CLOB);
+          ctx.assertEquals(result.columnDescriptors().get(9).name(), "MediumText");
+          ctx.assertEquals(result.columnDescriptors().get(9).jdbcType(), JDBCType.CLOB);
+          ctx.assertEquals(result.columnDescriptors().get(10).name(), "LongText");
+          ctx.assertEquals(result.columnDescriptors().get(10).jdbcType(), JDBCType.CLOB);
+          ctx.assertEquals(result.columnDescriptors().get(11).name(), "test_enum");
+          ctx.assertEquals(result.columnDescriptors().get(11).jdbcType(), JDBCType.CHAR);
+          ctx.assertEquals(result.columnDescriptors().get(12).name(), "test_set");
+          ctx.assertEquals(result.columnDescriptors().get(12).jdbcType(), JDBCType.CHAR);
+          ctx.assertEquals(result.columnDescriptors().get(13).name(), "test_year");
+          ctx.assertEquals(result.columnDescriptors().get(13).jdbcType(), JDBCType.SMALLINT);
+          ctx.assertEquals(result.columnDescriptors().get(14).name(), "test_timestamp");
+          ctx.assertEquals(result.columnDescriptors().get(14).jdbcType(), JDBCType.TIMESTAMP);
+          ctx.assertEquals(result.columnDescriptors().get(15).name(), "test_datetime");
+          ctx.assertEquals(result.columnDescriptors().get(15).jdbcType(), JDBCType.TIMESTAMP);
+          ctx.assertEquals(result.columnDescriptors().get(16).name(), "test_bit");
+          ctx.assertEquals(result.columnDescriptors().get(16).jdbcType(), JDBCType.BIT);
+          ctx.assertEquals(result.columnDescriptors().get(17).name(), "test_unsigned_tinyint");
+          ctx.assertEquals(result.columnDescriptors().get(17).jdbcType(), JDBCType.SMALLINT);
+          ctx.assertEquals(result.columnDescriptors().get(18).name(), "test_unsigned_smallint");
+          ctx.assertEquals(result.columnDescriptors().get(18).jdbcType(), JDBCType.INTEGER);
+          ctx.assertEquals(result.columnDescriptors().get(19).name(), "test_unsigned_mediumint");
+          ctx.assertEquals(result.columnDescriptors().get(19).jdbcType(), JDBCType.INTEGER);
+          ctx.assertEquals(result.columnDescriptors().get(20).name(), "test_unsigned_int");
+          ctx.assertEquals(result.columnDescriptors().get(20).jdbcType(), JDBCType.BIGINT);
+          ctx.assertEquals(result.columnDescriptors().get(21).name(), "test_unsigned_bigint");
+          ctx.assertEquals(result.columnDescriptors().get(21).jdbcType(), JDBCType.NUMERIC);
+          ctx.assertEquals(result.columnDescriptors().get(22).name(), "test_varchar_binary");
+          ctx.assertEquals(result.columnDescriptors().get(22).jdbcType(), JDBCType.VARCHAR);
+          ctx.assertEquals(result.columnDescriptors().get(23).name(), "test_varchar_with_binary_collation");
+          ctx.assertEquals(result.columnDescriptors().get(23).jdbcType(), JDBCType.VARBINARY);
+          ctx.assertEquals(result.columnDescriptors().get(24).name(), "test_text_binary");
+          ctx.assertEquals(result.columnDescriptors().get(24).jdbcType(), JDBCType.CLOB);
+        }));
+    }));
+  }
 }
