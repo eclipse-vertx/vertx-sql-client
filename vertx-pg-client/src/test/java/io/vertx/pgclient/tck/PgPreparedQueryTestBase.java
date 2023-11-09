cc9803f073f4f7ca7717e97f20d8fdc810ff32ab
Thomas Segismont <tsegismont@gmail.com>
2023-11-09 22:21:46 +0100

    Pg Client: fail cleanly when number of params exceeds limit (#1376)
    See 1142
    
    When the number of params exceeds the limit, Postgres successfully prepares the query but returns a number of expected equal to (actual - max - 1).
    
    Then the client reports a strange message like:
    
    "The number of parameters to execute should be consistent with the expected number of parameters = [64464] but the actual number is [130000]."
    
    With this change, an explicit error message is reported.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-pg-client/src/test/java/io/vertx/pgclient/tck/PgPreparedQueryTestBase.java
+++ vertx-pg-client/src/test/java/io/vertx/pgclient/tck/PgPreparedQueryTestBase.java
@@ -1,8 +1,11 @@
 package io.vertx.pgclient.tck;
 
+import io.vertx.ext.unit.TestContext;
 import io.vertx.pgclient.junit.ContainerPgRule;
+import io.vertx.sqlclient.Tuple;
 import io.vertx.sqlclient.tck.PreparedQueryTestBase;
 import org.junit.ClassRule;
+import org.junit.Test;
 
 public abstract class PgPreparedQueryTestBase extends PreparedQueryTestBase {
   @ClassRule
@@ -18,5 +21,30 @@ public abstract class PgPreparedQueryTestBase extends PreparedQueryTestBase {
       sb.append(parts[i]);
     }
     return sb.toString();
+  }
+
+  @Test
+  public void testMaximumParametersExceeded(TestContext ctx) {
+    int cnt = 33000;
+    StringBuilder valuesString = new StringBuilder(3 * 2 * cnt);
+    Object[] values = new Object[2 * cnt];
+    for (int i = 0; i < cnt; i++) {
+      values[2 * i] = i;
+      values[2 * i + 1] = "value-" + i;
+      if (i > 0) {
+        valuesString.append(',');
+      }
+      valuesString.append("($").append(2 * i + 1).append(',').append('$').append(2 * (i + 1)).append(')');
+    }
+    Tuple tuple = Tuple.wrap(values);
+    String query = String.format("INSERT INTO mutable (id, val) VALUES %s", valuesString);
+    connect(ctx.asyncAssertSuccess(conn -> {
+      conn.preparedQuery(query)
+        .execute(tuple)
+        .onComplete(ar -> {
+          ctx.assertTrue(ar.failed());
+          ctx.assertTrue(ar.cause().getMessage().contains("exceeds the maximum of 65535"));
+        });
+    }));
   }
 }
