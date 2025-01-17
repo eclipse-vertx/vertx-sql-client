eclipse-vertx/vertx-sql-client

commit d4e5c28b400c2eb71095594bad958dc7f75d9dd3

Pg Client might convert a query param to NULL if it has the wrong type (#1464)
Fixes #1463

With this change, a DataType always has a param extractor.
The default extractor signals the failure to convert to PgParamDesc.

Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

vertx-pg-client/src/main/java/io/vertx/pgclient/impl/codec/DataType.java
@@ -18,29 +18,22 @@ package io.vertx.pgclient.impl.codec;
 
 import io.netty.util.collection.IntObjectHashMap;
 import io.netty.util.collection.IntObjectMap;
+import io.vertx.core.VertxException;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.pgclient.data.Box;
-import io.vertx.pgclient.data.Circle;
-import io.vertx.pgclient.data.Inet;
-import io.vertx.pgclient.data.Line;
-import io.vertx.pgclient.data.LineSegment;
-import io.vertx.pgclient.data.Money;
-import io.vertx.pgclient.data.Cidr;
+import io.vertx.pgclient.data.*;
 import io.vertx.sqlclient.Tuple;
 import io.vertx.sqlclient.data.Numeric;
-import io.vertx.pgclient.data.Interval;
-import io.vertx.pgclient.data.Path;
-import io.vertx.pgclient.data.Point;
-import io.vertx.pgclient.data.Polygon;
-import io.vertx.core.buffer.Buffer;
+import io.vertx.sqlclient.impl.TupleInternal;
 
 import java.sql.JDBCType;
 import java.time.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -155,11 +148,11 @@ public enum DataType {
   <T> DataType(int id, boolean supportsBinary, Class<T> encodingType, Class<?> decodingType, JDBCType jdbcType, ParamExtractor<T> paramExtractor) {
     this.id = id;
     this.supportsBinary = supportsBinary;
-    this.encodingType = encodingType;
+    this.encodingType = Objects.requireNonNull(encodingType);
     this.decodingType = decodingType;
     this.jdbcType = jdbcType;
     this.array = decodingType.isArray();
-    this.paramExtractor = paramExtractor;
+    this.paramExtractor = paramExtractor != null ? paramExtractor : new DefaultParamExtractor<>(encodingType);
   }
 
   static DataType valueOf(int oid) {
@@ -230,5 +223,24 @@ public enum DataType {
     encodingTypeToDataType.put(Polygon[].class, POLYGON_ARRAY);
     encodingTypeToDataType.put(Circle.class, CIRCLE);
     encodingTypeToDataType.put(Circle[].class, CIRCLE_ARRAY);
+  }
+
+  private static class DefaultParamExtractor<T> implements ParamExtractor<T> {
+    static final RuntimeException FAILURE = new VertxException("ignored", true);
+
+    final Class<T> encodingType;
+
+    DefaultParamExtractor(Class<T> encodingType) {
+      this.encodingType = encodingType;
+    }
+
+    @Override
+    public T get(TupleInternal tuple, int idx) {
+      Object value = tuple.getValue(idx);
+      if (value != null && encodingType.isAssignableFrom(value.getClass())) {
+        return encodingType.cast(value);
+      }
+      throw FAILURE;
+    }
   }
 }

vertx-pg-client/src/main/java/io/vertx/pgclient/impl/codec/PgParamDesc.java
@@ -52,11 +52,7 @@ class PgParamDesc extends ParamDesc {
       ParamExtractor<?> extractor = paramDataType.paramExtractor;
       Object val;
       try {
-        if (extractor != null) {
-          val = extractor.get(values, i);
-        } else {
-          val = values.get(paramDataType.encodingType, i);
-        }
+        val = extractor.get(values, i);
       } catch (Exception e) {
         return ErrorMessageFactory.buildWhenArgumentsTypeNotMatched(paramDataType.decodingType, i, values.getValue(i));
       }

vertx-pg-client/src/test/java/io/vertx/pgclient/PreparedStatementTest.java
@@ -17,10 +17,34 @@
 
 package io.vertx.pgclient;
 
+import io.vertx.ext.unit.TestContext;
+import io.vertx.sqlclient.Tuple;
+import org.junit.Test;
+
+import java.time.Duration;
+
 public class PreparedStatementTest extends PreparedStatementTestBase {
 
   @Override
   protected PgConnectOptions options() {
     return new PgConnectOptions(options).setCachePreparedStatements(false);
+  }
+
+  @Test
+  public void testPrepareExecuteValidationErrorDefaultExtractor(TestContext ctx) {
+    PgConnection.connect(vertx, options(), ctx.asyncAssertSuccess(conn -> {
+      conn.prepare("SELECT $1 :: INTERVAL \"Interval\"", ctx.asyncAssertSuccess(ps -> {
+        ps.query().execute(Tuple.of(Duration.ofHours(3)), ctx.asyncAssertFailure());
+      }));
+    }));
+  }
+
+  @Test
+  public void testPrepareExecuteValidationErrorDefaultExtractor_(TestContext ctx) {
+    PgConnection.connect(vertx, options(), ctx.asyncAssertSuccess(conn -> {
+      conn
+        .preparedQuery("SELECT $1 :: INTERVAL \"Interval\"")
+        .execute(Tuple.of(Duration.ofHours(3)), ctx.asyncAssertFailure());
+    }));
   }
 }

commit b89487ce1eb05fcba78e9d78cc50b2b24a165d35

PgClient: NPE when inserting null param with default param extractor
See #1477

This is a regression introduced in #1464

When the value is null, the extractor throws a failure.
Then, in ErrorMessageFactory.buildWhenArgumentsTypeNotMatched, a NPE is thrown because the value of the class cannot be determined.

Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

vertx-pg-client/src/main/java/io/vertx/pgclient/impl/codec/DataType.java
@@ -237,7 +237,10 @@ public enum DataType {
     @Override
     public T get(TupleInternal tuple, int idx) {
       Object value = tuple.getValue(idx);
-      if (value != null && encodingType.isAssignableFrom(value.getClass())) {
+      if (value == null) {
+        return null;
+      }
+      if (encodingType.isAssignableFrom(value.getClass())) {
         return encodingType.cast(value);
       }
       throw FAILURE;

vertx-pg-client/src/test/java/io/vertx/pgclient/data/PreparedStatementParamCoercionTest.java
@@ -1,9 +1,9 @@
 package io.vertx.pgclient.data;
 
-import io.vertx.pgclient.PgConnection;
-import io.vertx.sqlclient.Tuple;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
+import io.vertx.pgclient.PgConnection;
+import io.vertx.sqlclient.Tuple;
 import io.vertx.sqlclient.data.Numeric;
 import org.junit.Test;
 
@@ -65,6 +65,18 @@ public class PreparedStatementParamCoercionTest extends DataTypeTestBase {
           .execute(Tuple.of("not-an-uuid"))
           .onComplete(ctx.asyncAssertFailure(res -> {
         }));
+      }));
+    }));
+  }
+
+  @Test
+  public void testNoCoercionErrorWithNull(TestContext ctx) {
+    PgConnection.connect(vertx, options).onComplete(ctx.asyncAssertSuccess(conn -> {
+      conn.prepare("SELECT $1::POINT").onComplete(ctx.asyncAssertSuccess(pq -> {
+        pq
+          .query()
+          .execute(Tuple.of(null))
+          .onComplete(ctx.asyncAssertSuccess());
       }));
     }));
   }