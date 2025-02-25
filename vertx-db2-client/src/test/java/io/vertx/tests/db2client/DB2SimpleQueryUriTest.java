c9a033479c5da46c05d6587233bbe87900126848
Julien Viet <julien@julienviet.com>
2025-02-25 16:29:45 +0100

    Refactor package names of vertx-db2-client

---
+++ vertx-db2-client/src/test/java/io/vertx/tests/db2client/DB2SimpleQueryUriTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.vertx.tests.db2client;
+
+import io.vertx.db2client.DB2Connection;
+import org.junit.runner.RunWith;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.tests.db2client.tck.DB2SimpleQueryTest;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.sqlclient.SqlConnection;
+import io.vertx.tests.sqlclient.tck.Connector;
+
+/*
+ * Runs the tck SimpleQueryTest, but with a uri instead of connection properties.
+ */
+@RunWith(VertxUnitRunner.class)
+public class DB2SimpleQueryUriTest extends DB2SimpleQueryTest {
+
+  @Override
+  protected void initConnector() {
+    String uri = "db2://"+rule.options().getUser()+":"
+                         +rule.options().getPassword()+"@"
+                         +rule.options().getHost()+":"
+                         +rule.options().getPort()+"/"
+                         +rule.options().getDatabase();
+
+    connector = new Connector<SqlConnection>() {
+      @Override
+      public void connect(Handler<AsyncResult<SqlConnection>> handler) {
+        DB2Connection.connect(vertx, uri).onComplete(ar -> {
+          if (ar.succeeded()) {
+            handler.handle(Future.succeededFuture(ar.result()));
+          } else {
+            handler.handle(Future.failedFuture(ar.cause()));
+          }
+        });
+      }
+
+      @Override
+      public void close() {
+      }
+    };
+  }
+}
---
+++ vertx-db2-client/src/test/java/io/vertx/tests/db2client/tck/DB2SimpleQueryPooledTest.java
@@ -0,0 +1,43 @@
+package io.vertx.tests.db2client.tck;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+
+import io.vertx.tests.db2client.junit.DB2Resource;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.tests.sqlclient.tck.SimpleQueryTestBase;
+
+@RunWith(VertxUnitRunner.class)
+public class DB2SimpleQueryPooledTest extends SimpleQueryTestBase {
+    @ClassRule
+    public static DB2Resource rule = DB2Resource.SHARED_INSTANCE;
+
+  @Rule
+  public TestName testName = new TestName();
+
+  @Before
+  public void printTestName(TestContext ctx) throws Exception {
+    System.out.println(">>> BEGIN " + getClass().getSimpleName() + "." + testName.getMethodName());
+  }
+
+    @Override
+    protected void initConnector() {
+        connector = ClientConfig.POOLED.connect(vertx, rule.options());
+    }
+
+    @Override
+    protected void cleanTestTable(TestContext ctx) {
+        connect(ctx.asyncAssertSuccess(conn -> {
+            conn
+              .query("DELETE FROM mutable")
+              .execute()
+              .onComplete(ctx.asyncAssertSuccess(result -> {
+                conn.close();
+            }));
+        }));
+    }
+}
