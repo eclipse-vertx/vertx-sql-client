743d2796b3a11c112c4db0e81c6f66705251d22a
Thomas Segismont <tsegismont@gmail.com>
2025-10-21 19:07:04 +0200

    Indeterminate PreparedStatement error hidden when inside a transaction (#1558) (#1560)
    See #1557
    
    When inside a transaction (autocommit=false), don't try to prepare a statement again, even if the error is due to indeterminate types.
    
    Indeed, the transaction is aborted and new commands are ignored until the transaction is rollbacked.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-pg-client/src/test/java/io/vertx/pgclient/PreparedStatementTestBase.java
+++ vertx-pg-client/src/test/java/io/vertx/pgclient/PreparedStatementTestBase.java
@@ -509,6 +509,14 @@ public abstract class PreparedStatementTestBase extends PgTestBase {
           ctx.assertEquals("HELLO " + suffix2, str);
         }));
     }));
+    PgConnection.connect(vertx, options()).onComplete(ctx.asyncAssertSuccess(conn -> {
+      conn.begin()
+        .flatMap(tx -> conn.preparedQuery("SELECT CONCAT('HELLO ', $1)").execute(Tuple.of(value))
+          .eventually(() -> conn.close())
+          .onComplete(ctx.asyncAssertFailure(failure -> {
+            ctx.assertTrue(hasSqlstateCode(failure, "42P18"));
+          })));
+    }));
   }
 
   @Test
