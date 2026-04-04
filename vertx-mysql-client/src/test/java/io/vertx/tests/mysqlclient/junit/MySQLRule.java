06b2dbd7e4040c6e12cf2c8a2400d616f38ac097
Thomas Segismont <tsegismont@gmail.com>
2026-04-04 10:39:41 +0200

    Fix MySQLRule for MariaDB 11.8
    caching-sha2-password-public-key-path and caching-sha2-password-private-key-path are MySQL specific now.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-mysql-client/src/test/java/io/vertx/tests/mysqlclient/junit/MySQLRule.java
+++ vertx-mysql-client/src/test/java/io/vertx/tests/mysqlclient/junit/MySQLRule.java
@@ -20,6 +20,7 @@ import org.testcontainers.containers.Network;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.StringJoiner;
 
 public class MySQLRule extends ExternalResource {
   private static final String connectionUri = System.getProperty("connection.uri");
@@ -93,9 +94,17 @@ public class MySQLRule extends ExternalResource {
       server.withClasspathResourceMapping("tls/files", "/etc/mysql/tls", BindMode.READ_ONLY);
     } else {
       server.withClasspathResourceMapping("tls/files", "/etc/mysql/tls", BindMode.READ_ONLY);
-      String cmd = "--max_allowed_packet=33554432 --max_prepared_stmt_count=1024 --local_infile=true --character-set-server=utf8mb4 --collation-server=utf8mb4_general_ci";
-      cmd += " --caching-sha2-password-public-key-path=/etc/mysql/tls/public_key.pem --caching-sha2-password-private-key-path=/etc/mysql/tls/private_key.pem";
-      server.withCommand(cmd);
+      StringJoiner cmd = new StringJoiner(" ");
+      cmd.add("--max_allowed_packet=33554432");
+      cmd.add("--max_prepared_stmt_count=1024");
+      cmd.add("--local_infile=true");
+      cmd.add("--character-set-server=utf8mb4");
+      cmd.add("--collation-server=utf8mb4_general_ci");
+      if (databaseServerInfo.databaseType == DatabaseType.MySQL) {
+        cmd.add("--caching-sha2-password-public-key-path=/etc/mysql/tls/public_key.pem");
+        cmd.add("--caching-sha2-password-private-key-path=/etc/mysql/tls/private_key.pem");
+      }
+      server.withCommand(cmd.toString());
     }
   }
 
